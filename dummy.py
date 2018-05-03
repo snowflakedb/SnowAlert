1c5ade93723ce7f86b5674076f5ef9cb2eb0babd
Omer Singer <>
2018-05-03 11:25:27 -0700

    clean up dummy file

--- dummy.py
+++ dummy.py
@@ -1,3 +1,3 @@
 import plugins
 
-plugins.create_jira_ticket('777', 'sometime', '1', 'deeee', 'here', 'sometype', 'macbookpro1', 'trojan', 'some desc')
+print(plugins.create_jira_ticket('777', 'sometime', '1', 'deeee', 'here', 'sometype', 'macbookpro1', 'trojan', 'some desc'))
