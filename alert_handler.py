5fca671d8276eea6575860077b2bec97beb066ba
Omer Singer <>
2018-05-02 17:31:46 -0700

    getting new alerts, creating a ticket in JIRA and recording the ticket number in the table

---
+++ alert_handler.py
@@ -0,0 +1,50 @@
+import os
+import json
+import snowflake.connector
+import plugins
+
+
+def db_connect():
+    connection = snowflake.connector.connect(
+        account=os.environ['SNOWALERT_ACCOUNT'],
+        user=os.environ['SNOWALERT_USER'],
+        password=os.environ['SNOWALERT_PASSWORD']
+    )
+    connection.cursor().execute('use warehouse snowalert')
+    connection.cursor().execute('use database snowalert')
+    return connection
+
+
+def get_new_alerts(connection):
+    alert_query = 'select * from alerts where ticket is null'
+
+    results = connection.cursor().execute(alert_query).fetchall()
+    return results
+
+
+def record_ticket_id(connection, ticket_id, guid):
+    query = "update alerts set ticket = '" + str(ticket_id) + "' where alert:guid = '" + str(guid) + "'"
+    print('debug:', query)
+    connection.cursor().execute(query)
+
+
+if __name__ == "__main__":
+    ctx = db_connect()
+
+    for row in get_new_alerts(ctx):
+        alert = json.loads(row[0])
+
+        # Create a new ticket in JIRA for the alert
+        ticketID = plugins.create_jira_ticket(
+            guid=alert['guid'],                     # TODO: alert generators should assign guid
+            creationTime=alert['timestamp'],
+            severity=alert['severity'],
+            detector=alert['detector'],
+            env=alert['environment'],
+            objectType=alert['object_type'],
+            object=alert['object'],
+            alertType=alert['alert_type'],
+            description=alert['description'])
+
+        # Record the new ticket id in the alert table
+        record_ticket_id(ctx, ticketID, alert['guid'])
