91b160230edf23e79bb3eac2e0d42c09c399e5c2
Omer Singer <omer.singer@snowflake.net>
2018-05-29 17:09:09 -0700

    adding pages

--- docs/conf.py
+++ docs/conf.py
@@ -20,8 +20,8 @@
 # -- Project information -----------------------------------------------------
 
 project = 'SnowAlert'
-copyright = '2018, Snowflake Security'
-author = 'Snowflake Security'
+copyright = ''
+author = ''
 
 # The short X.Y version
 version = ''
@@ -74,7 +74,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
