0897919e06b9f38effff54803bdc7e1926cfdaf0
Andrey Fedorov <andrey.fedorov@snowflake.com>
2018-10-19 12:29:06 -0700

    Adds log.info usage and simplifies log.write

--- src/helpers/log.py
+++ src/helpers/log.py
@@ -3,16 +3,12 @@ import sys
 
 
 def write(*args, stream=sys.stdout):
-    if all(isinstance(a, str) for a in args):
-        print(*args, sep='\n', file=stream, flush=True)
-        traceback.print_exc(file=stream)
-    else:
-        for a in args:
-            if isinstance(a, Exception):
-                traceback.print_exception(type(a), a, a.__traceback__, file=stream)
-                stream.flush()
-            else:
-                print(a, file=stream, flush=True)
+    for a in args:
+        if isinstance(a, Exception):
+            traceback.print_exception(type(a), a, a.__traceback__, file=stream)
+            stream.flush()
+        else:
+            print(a, file=stream, flush=True)
 
 
 def info(*args):
