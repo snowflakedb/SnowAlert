34cff8e3d7b2355408f34363fe113ba1f043f513
Andrey Fedorov <andrey.fedorov@snowflake.com>
2019-01-02 19:04:05 -0800

    Adds Policies section to view Policy rules & results

--- src/samui/frontend/src/constants/routes.ts
+++ src/samui/frontend/src/constants/routes.ts
@@ -5,6 +5,7 @@ export const LOGIN = '/user/login';
 export const REGISTER = '/user/register';
 export const REGISTER_RESULT = '/user/register-result';
 
+export const POLICIES = '/dashboard/policies';
 export const ALERTS = '/dashboard/alerts';
 export const VIOLATIONS = '/dashboard/violations';
 
