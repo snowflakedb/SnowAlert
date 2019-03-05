cc7777632d7ceb57b56438b129d1b5014fc1a825
Andrey Fedorov <andrey.fedorov@snowflake.com>
2019-03-04 17:54:30 -0800

    Update mypy version and setup.py package finder

--- src/setup.py
+++ src/setup.py
@@ -1,15 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="snowalert-runners",
     version='1.0',
-    packages=['runners', 'runners.helpers', 'runners.plugins'],
+    packages=find_packages(),
     install_requires=[
         'jira==2.0.0',
         'PyYAML==4.2b1',
         'snowflake-connector-python==1.7.3',
         'snowflake-sqlalchemy==1.1.2',
-        'mypy==0.650',
+        'mypy==0.670',
         'mypy-extensions==0.4.1',
     ],
 )
