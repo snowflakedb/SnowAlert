af42152764d7169b87d9bdddc647cd17362cd4bc
Andrey Fedorov <andrey.fedorov@snowflake.com>
2019-03-07 19:10:51 -0800

    Fixes mypy configuration and cleans up vestigial code

--- src/samui/backend/setup.py
+++ src/samui/backend/setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup, find_packages
 
-
 setup(
     name="samui",
     version='1.0',
--- src/setup.py
+++ src/setup.py
@@ -7,7 +7,7 @@ setup(
     install_requires=[
         'jira==2.0.0',
         'PyYAML==4.2b1',
-        'snowflake-connector-python==1.7.3',
+        'snowflake-connector-python==1.7.7',
         'snowflake-sqlalchemy==1.1.2',
         'mypy==0.670',
         'mypy-extensions==0.4.1',
