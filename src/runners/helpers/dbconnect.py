30dd653fa17be880aa098c177a85c1c9b54a13d2
Andrey Fedorov <andrey.fedorov@snowflake.com>
2019-03-21 16:32:20 -0700

    Clarifies naming of dbconnect patches

--- src/runners/helpers/dbconnect.py
+++ src/runners/helpers/dbconnect.py
@@ -4,23 +4,23 @@ from unittest.mock import patch
 from snowflake.connector.connection import SnowflakeConnection as BadSnowflakeConnection
 
 
-def open_new(url):
+def newer_open_new(url):
     print(f'--- Click below ---\n{url}\n- - - - - - - - - -', flush=True)
     return True
 
 
-class SocketPkg(socket.socket):
+class BetterSocket(socket.socket):
     def __init__(self, *args, **kwargs):
-        return super(SocketPkg, self).__init__(*args, **kwargs)
+        return super(BetterSocket, self).__init__(*args, **kwargs)
 
     def close(self, *args, **kwargs):
-        super(SocketPkg, self).close(*args, **kwargs)
+        super(BetterSocket, self).close(*args, **kwargs)
 
     def bind(self, address):
-        return super(SocketPkg, self).bind(('0.0.0.0', 1901))
+        return super(BetterSocket, self).bind(('0.0.0.0', 1901))
 
 
-@patch('socket.socket', new=SocketPkg)
-@patch('webbrowser.open_new', new=open_new)
+@patch('socket.socket', new=BetterSocket)
+@patch('webbrowser.open_new', new=newer_open_new)
 def snowflake_connect(**kwargs):
     return BadSnowflakeConnection(**kwargs)
