ad4bc5f3607d5bff09f9a5cb3ee45334fa0bf770
Andrey Fedorov <andrey.fedorov@snowflake.com>
2019-04-15 19:18:35 -0700

    Fixes baselines insert statement to properly escape values

--- src/runners/baseline_runner.py
+++ src/runners/baseline_runner.py
@@ -1,5 +1,7 @@
+from typing import List, Dict, Any
+
 from runners.config import (
-    DATA_SCHEMA_NAME,
+    DATA_SCHEMA,
 )
 
 from runners.helpers import db, log
@@ -11,97 +13,60 @@ import pandas
 import yaml
 
 
-def read_metadata(ctx):
-    query = f"""show tables like '%_BASELINE' in snowalert.{DATA_SCHEMA_NAME}"""
-    try:
-        rows = db.fetch(ctx, query)
-        return list(rows)
-    except Exception as e:
-        log.error("Unable to read metadata: ", e)
-        return None
-
-
-def format_code(r, vars):
-    ret = r
+def format_code(code, vars):
     for k, v in vars.items():
-        ret = ret.replace(k, v)
+        code = code.replace(k, v)
+    return code
 
-    return ret
 
-
-def pack(data):
-    f = {}
-    for row in data:
-        if len(f) == 0:
-            for k in row.keys():
-                f.update({k: []})
-
-        for k in row:
-            f[k].append(row[k])
-
-    return f
+def pack(data: List[Dict[Any, Any]]) -> Dict[Any, List[Any]]:
+    keys = {k for row in data for k in row.keys()}
+    return {k: [d.get(k) for d in data] for k in keys}
 
 
 def unpack(data):
     b = [[data[k][i] for i in data[k]] for k in data]
-    output = list(zip(*b))
-    return output
+    return list(zip(*b))
 
 
-def query_log_source(ctx, source, time_filter, history):
-    log.info("Querying log source and packing data into frame")
-    query = f"""SELECT * from {source} where {history} > dateadd(day, -{time_filter}, current_timestamp());"""
-    if ctx is not None:
-        try:
-            data = db.fetch(ctx, query)
-        except Exception as e:
-            log.error("Failed to query log source: ", e)
-    else:
-        log.error("No connection passed in for log source query.")
-        return None
+def query_log_source(source, time_filter, time_column):
+    cutoff = f"DATEADD(day, -{time_filter}, CURRENT_TIMESTAMP()"
+    query = f"SELECT * FROM {source} WHERE {time_column} > {cutoff};"
+    try:
+        data = db.fetch(query)
+    except Exception as e:
+        log.error("Failed to query log source: ", e)
 
     f = pack(data)
-
     frame = pandas.DataFrame(f)
     pandas2ri.activate()
     r_dataframe = pandas2ri.py2rpy(frame)
     return r_dataframe
 
 
-def log_results_query(target, results):
-    query = f"""insert into snowalert.{DATA_SCHEMA_NAME}.{target} values """
-    for i in results:
-        query += str(i) + ", "
-
-    query = query[:-2]
-    return query
-
-
-def run_baseline(ctx, row):
+def run_baseline(table):
+    table_name = table['name']
     try:
-        metadata = yaml.load(row['comment'])
-    except Exception:
-        log.error("Failed to load metadata from comment")
-        return None
+        metadata = yaml.load(table['comment'])
+        assert type(metadata) is dict
 
-    if metadata is None:
-        log.error(f"No comment for table {row['name']}")
-        return None
+        source = metadata['log source']
+        required_values = metadata['required values']
+        code_location = metadata['module name']
+        time_filter = metadata['filter']
+        time_column = metadata['history']
 
-    log_source = metadata['log source']
-    required_values = metadata['required values']
-    output_table = row['name']
-    code_location = metadata['module name']
-    time_filter = metadata['filter']
-    history = metadata['history']
+    except Exception as e:
+        log.error(e, "{table_name} has invalid metadata, skipping")
+        return
 
     log.info("Reading R code")
 
-    with open(f"../baseline_modules/{code_location}/{code_location}.R", "r") as f:
+    with open(f"../baseline_modules/{code_location}/{code_location}.R") as f:
         r_code = f.read()
 
     r_code = format_code(r_code, required_values)
-    frame = query_log_source(ctx, log_source, time_filter, history)
+    frame = query_log_source(source, time_filter, time_column)
     ro.globalenv['input_table'] = frame
 
     log.info("Running R code")
@@ -111,17 +76,14 @@ def run_baseline(ctx, row):
 
     results = unpack(output)
     try:
-        db.execute(ctx, f'truncate table snowalert.{DATA_SCHEMA_NAME}.{output_table};')
-        db.execute(ctx, log_results_query(output_table, results))
+        db.insert(f"{DATA_SCHEMA}.{table_name}", results, ovewrite=True)
     except Exception as e:
         log.error("Failed to insert the results into the target table", e)
 
 
 def main():
-    ctx = db.connect()
-    log.info("Finding baseline metadata")
-    tables = read_metadata(ctx)
-
-    for row in tables:
-        log.info(f"Starting baseline: {row['name']}")
-        run_baseline(ctx, row)
+    db.connect()
+    for table in db.fetch(f"show tables like '%_BASELINE' in {DATA_SCHEMA}"):
+        log.info(f'{table} started...')
+        run_baseline(table)
+        log.info(f'{table} done.')
