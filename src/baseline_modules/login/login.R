bfcff4e11d9f6de24eb52c4800b333a5d315b309
Maria Polyakova <maria.polyakova@snowflake.com>
2019-04-17 17:12:05 -0700

    Added descrirption to print statnement, changed nrow to length because it measures length of everything not just datafrrames

--- src/baseline_modules/login/login.R
+++ src/baseline_modules/login/login.R
@@ -1,11 +1,11 @@
 require('dplyr')
 results <- input_table
-print(nrow(results))
+print(paste('Length of input: ',len(input_table)))
 num_days_total <- length(unique(results$day))
 grouped <- results %>% group_by(pivot) %>% summarize(num_logins=length(event_time), num_unique_users=length(unique(user_id)), num_successful_logins=length(which(login_status=='Success')), num_days=length(unique(day)), percent_of_days=num_days/num_days_total)
 grouped$average_per_day_when_active = grouped$num_successful_logins/grouped$num_days
 grouped$average_per_day_overall = grouped$num_successful_logins/num_days_total
-print(nrow(grouped))
+print(paste('Length of output: ',len(grouped)))
 return_value <- grouped
 
 
