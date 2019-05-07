d06507e18831b107c6da19c05c1a24547c0c94bd
Andrey Fedorov <andrey.fedorov@snowflake.com>
2019-05-07 12:36:24 -0400

    Fixes buggy pk pass decryption

--- src/runners/helpers/kms.py
+++ src/runners/helpers/kms.py
@@ -7,7 +7,7 @@ from .dbconfig import REGION
 kms = boto3.client('kms', region_name=REGION)
 
 
-def decrypt_if_encrypted(ct, handleErrors=True):
+def decrypt_if_encrypted(ct, handleErrors=True, as_bytes=False) -> bytes:
     if not ct or len(ct) < 205:  # 1-byte plaintext has 205-byte ct
         return ct
 
@@ -18,7 +18,8 @@ def decrypt_if_encrypted(ct, handleErrors=True):
 
     try:
         # depending on local AWS config, this might ask for 2FA
-        return kms.decrypt(CiphertextBlob=ctBlob)['Plaintext'].decode()
+        pt = kms.decrypt(CiphertextBlob=ctBlob)['Plaintext']
+        return pt if as_bytes else pt.decode()
     except Exception:
         if handleErrors:
             return ct
