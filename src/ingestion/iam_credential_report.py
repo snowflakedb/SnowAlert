e8be0ed419827a754b2a8c82e6ded5b399f311b8
Soumitr Pandey <soumitr.pandey@snowflake.net>
2019-05-08 18:58:58 -0700

    improve logging and fix ec2 describe instances not recording all regions

--- src/ingestion/iam_credential_report.py
+++ src/ingestion/iam_credential_report.py
@@ -21,6 +21,8 @@ SA_USER = os.environ['INGEST_SNOWFLAKE_USER']
 SA_USER_PK = os.environ['INGEST_SNOWFLAKE_USER_PRIVATE_KEY']
 SA_USER_PK_PASSWORD = os.environ['INGEST_SNOWFLAKE_USER_PRIVATE_KEY_PASSWORD']
 CACHED_AWS_CLIENT = None
+ASSUME_ROLE_ERROR_SET = ()
+PASSWORD_POLICY_ERROR_SET = ()
 
 
 def get_snowflake_client():
@@ -65,14 +67,11 @@ def get_aws_client(account):
     if CACHED_AWS_CLIENT is None:
         CACHED_AWS_CLIENT = get_cached_aws_client()
     target_role = f'arn:aws:iam::{account}:role/{AWS_AUDIT_ROLE_NAME}'
-    try:
-        dest_role = CACHED_AWS_CLIENT.assume_role(
+    dest_role = CACHED_AWS_CLIENT.assume_role(
             RoleArn=target_role,
             RoleSessionName=os.environ['IAM_CREDENTIAL_REPORTS_AWS_AUDIT_DESTINATION_ROLE_SESSION_NAME'],
             ExternalId=os.environ['AWS_AUDIT_DESTINATION_ROLE_ARN_EXTERNALID']
         )
-    except Exception as e:
-        return None
     iam_session = boto3.Session(
         aws_access_key_id=dest_role['Credentials']['AccessKeyId'],
         aws_secret_access_key=dest_role['Credentials']['SecretAccessKey'],
@@ -82,7 +81,7 @@ def get_aws_client(account):
 
 
 GET_ACCOUNTS_QUERY = f"""
-SELECT account:Id::string
+SELECT account:Id::string, account:Name
 FROM {AWS_ACCOUNTS_TABLE}
 WHERE timestamp = (
   SELECT MAX(timestamp) FROM {AWS_ACCOUNTS_TABLE}
@@ -91,11 +90,8 @@ WHERE timestamp = (
 
 
 def get_accounts_list(sf_client):
-    accounts_list = []
     res = sf_client.cursor().execute(GET_ACCOUNTS_QUERY).fetchall()
-    for account in res:
-        accounts_list.append(account[0])
-    return accounts_list
+    return res
 
 
 LOAD_REPORT_LIST_QUERY = f"""
@@ -112,8 +108,8 @@ FROM VALUES {{format_string}}
 
 
 def get_data_worker(account):
-    iam_session = get_aws_client(account)
-    if iam_session:
+    try:
+        iam_session = get_aws_client(account[0])
         reports = []
         reports_json = []
         iam_client = iam_session.client('iam')
@@ -123,17 +119,23 @@ def get_data_worker(account):
                 r = iam_client.get_credential_report()
                 break
             except Exception as e:
+                print(f"iam_credential_report: account: {account[1]} exception: {e}")
                 time.sleep(2)
         try:
             policy = iam_client.get_account_password_policy()['PasswordPolicy']
         except Exception as e:
+            print(f"iam_credential_report: account: {account[1]} exception: {e}")
             policy = {}
         report = r['Content'].decode('utf-8').split('\n')
         data = [i.split(',') for i in report]
         reports = [dict(zip(data[0], i)) for i in data[1:]]
-        policy['AccountId'] = account
-        reports_json = [json.dumps({**report, "account_id": account}) for report in reports]
+        policy['AccountId'] = account[0]
+        reports_json = [json.dumps({**report, "account_id": account[0]}) for report in reports]
+        print(f"iam_credential_report: account: {account[1]} credentials: {len(reports_json)}")
         return {'report': reports_json, 'policy': json.dumps(policy)}
+    except Exception as e:
+        print(f"iam_credential_report: account: {account[1]} exception: {e}")
+        return None
 
 def get_data(accounts_list):
     start = datetime.datetime.now()
@@ -142,22 +144,23 @@ def get_data(accounts_list):
         policies_list = [result['policy'] for result in results_list if result]
         reports_list = [report for result in results_list if result for report in result['report']]
         sf_client = get_snowflake_client()
+        snapshot_time = datetime.datetime.utcnow().isoformat()
         if reports_list:
             report_groups = groups_of(15000,reports_list)
             for group in report_groups:
                 query = LOAD_REPORT_LIST_QUERY.format(
-                    snapshotclock=datetime.datetime.utcnow().isoformat(),
+                    snapshotclock=snapshot_time,
                     format_string=", ".join(["(%s)"] * len(group)))
                 sf_client.cursor().execute(query, group)
         if policies_list:
             policy_groups = groups_of(15000,policies_list)
             for group in policy_groups:
                 query = LOAD_POLICY_LIST_QUERY.format(
-                    snapshotclock=datetime.datetime.utcnow().isoformat(),
+                    snapshotclock=snapshot_time,
                     format_string=", ".join(["(%s)"] * len(group)))
                 sf_client.cursor().execute(query, group)
     end = datetime.datetime.now()
-    print(f"start: {start} end: {end} total: {(end - start).total_seconds()}")
+    print(f"iam_credential_report: start: {start} end: {end} total: {(end - start).total_seconds()}")
 
 
 def main():
@@ -168,7 +171,7 @@ def main():
         accounts_list = get_accounts_list(sf_client)
         get_data(accounts_list)
     else:
-        print("It's not time yet!")
+        print("iam_credential_report: It's not time yet!")
 
 
 if __name__ == "__main__":
