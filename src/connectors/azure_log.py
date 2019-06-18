db820f2cc084d66ef4b7e33a0f44ba9257b804cf
Andrey Fedorov <andrey.fedorov@snowflake.com>
2019-06-18 11:31:34 -0700

    Fixes azure_log connector + adds icon

--- src/connectors/azure_log.py
+++ src/connectors/azure_log.py
@@ -38,7 +38,10 @@ CONNECTION_OPTIONS = [
         'title': 'SAS Token',
         'prompt': "A SAS Token which can list and read the files in the blob.",
         'secret': True,
-        'placeholder': '?sv=2010-01-01&ss=abcd&srt=def&sp=gh&se=2011-01-01T00:12:34Z&st=2011-01-23T45:67:89Z&spr=https&sig=abcdefghijklmnopqrstuvwxyz%3D',
+        'placeholder': (
+            '?sv=2010-01-01&ss=abcd&srt=def&sp=gh&se=2011-01-01T00:12:34Z'
+            '&st=2011-01-23T45:67:89Z&spr=https&sig=abcdefghijklmnopqrstuvwxyz%3D'
+        ),
         'required': True
     },
     {
@@ -214,59 +217,68 @@ COPY INTO DATA.{name}_CONNECTION(RAW, HASH_RAW, CALLER_IP_ADDRESS, CATEGORY, COR
                                  PROPERTIES_POLICIES, PROPERTIES_RESOURCE_LOCAATION, RESOURCE_ID, RESULT_SIGNATURE,
                                  RESULT_TYPE, EVENT_TIME, LOADED_ON)
 FROM (
-SELECT $1, HASH($1), $1:callerIpAddress::STRING, $1:category::STRING, $1:correlationId::STRING, $1:durationMs::NUMBER,
-       $1:identity::VARIANT, $1:identity.authorization::VARIANT, $1:identity.claims::VARIANT, $1:level::STRING,
-       $1:location::STRING, $1:operationName::STRING, $1:properties::VARIANT, $1:properties.ancestors::STRING,
-       $1:properties.isComplianceCheck::STRING, PARSE_JSON($1:properties.policies), $1:properties.resourceLocation::STRING,
-       $1:resourceId::STRING, $1:resultSignature::STRING, $1:resultType::STRING, $1:time::TIMESTAMP_LTZ, CURRENT_TIMESTAMP()
-FROM @DATA.{name}_STAGE)
+    SELECT $1, HASH($1), $1:callerIpAddress::STRING, $1:category::STRING, $1:correlationId::STRING,
+        $1:durationMs::NUMBER, $1:identity::VARIANT, $1:identity.authorization::VARIANT, $1:identity.claims::VARIANT,
+        $1:level::STRING, $1:location::STRING, $1:operationName::STRING, $1:properties::VARIANT,
+        $1:properties.ancestors::STRING, $1:properties.isComplianceCheck::STRING, PARSE_JSON($1:properties.policies),
+        $1:properties.resourceLocation::STRING, $1:resourceId::STRING, $1:resultSignature::STRING,
+        $1:resultType::STRING, $1:time::TIMESTAMP_LTZ, CURRENT_TIMESTAMP()
+    FROM @DATA.{name}_STAGE)
 """,
         'audit': f"""
 COPY INTO data.{name}_CONNECTION (RAW, HASH_RAW, CALLER_IP_ADDRESS, CATEGORY, CORRELATION_ID,
                                   DURATION_MS, LEVEL, OPERATION_NAME, OPERATION_VERSION, PROPERTIES,
                                   PROPERTIES_ACTIVITY_DATE_TIME, PROPERTIES_ACTIVITY_DISPLAY_NAME,
-                                  PROPERTIES_ADDITIONAL_DETAILS, PROPERTIES_CATEGORY, PROPERTIES_ID, PROPERTIES_INITIATED_BY,
-                                  PROPERTIES_LOGGED_BY_SERVICE, PROPERTIES_OPERATION_TYPE, PROPERTIES_RESULT,
-                                  PROPERTIES_RESULT_REASON, PROPERTIES_TARGET_RESOURCES,
+                                  PROPERTIES_ADDITIONAL_DETAILS, PROPERTIES_CATEGORY, PROPERTIES_ID,
+                                  PROPERTIES_INITIATED_BY, PROPERTIES_LOGGED_BY_SERVICE, PROPERTIES_OPERATION_TYPE,
+                                  PROPERTIES_RESULT, PROPERTIES_RESULT_REASON, PROPERTIES_TARGET_RESOURCES,
                                   RESOURCE_ID, RESULT_SIGNATURE, TENANT_ID, EVENT_TIME, LOADED_ON)
 FROM (
-SELECT $1, HASH($1), $1:callerIpAddress::STRING, $1:category::STRING, $1:correlationId::STRING, $1:durationMs::NUMBER,
-       $1:level::STRING, $1:operationName::STRING, $1:operationVersion::STRING, $1:properties::VARIANT, $1:properties.activityDateTime::TIMESTAMP_LTZ,
-       $1:properties.activityDisplayName::STRING, $1:properties.additionalDetails::VARIANT, $1:properties.category::STRING, $1:properties.id::STRING,
-       $1:properties.initiatedBy::VARIANT, $1:properties.loggedByService::STRING, $1:properties.operationType::STRING, $1:properties.result::STRING,
-       $1:resultReason::STRING, $1:properties.targetResources::VARIANT, $1:resourceId::STRING, $1:resultSignature::STRING, $1:tenantId::STRING,
-       $1:time::TIMESTAMP_LTZ, CURRENT_TIMESTAMP()
-FROM @data.{name}_stage
+    SELECT $1, HASH($1), $1:callerIpAddress::STRING, $1:category::STRING, $1:correlationId::STRING,
+        $1:durationMs::NUMBER, $1:level::STRING, $1:operationName::STRING, $1:operationVersion::STRING,
+        $1:properties::VARIANT, $1:properties.activityDateTime::TIMESTAMP_LTZ,
+        $1:properties.activityDisplayName::STRING, $1:properties.additionalDetails::VARIANT,
+        $1:properties.category::STRING, $1:properties.id::STRING, $1:properties.initiatedBy::VARIANT,
+        $1:properties.loggedByService::STRING, $1:properties.operationType::STRING, $1:properties.result::STRING,
+        $1:resultReason::STRING, $1:properties.targetResources::VARIANT, $1:resourceId::STRING,
+        $1:resultSignature::STRING, $1:tenantId::STRING, $1:time::TIMESTAMP_LTZ, CURRENT_TIMESTAMP()
+  FROM @data.{name}_stage
 )
 """,
         'signin': f"""
-COPY INTO DATA.{name}_CONNECTION (RAW, HASH_RAW, LEVEL, CALLER_IP_ADDRESS, CATEGORY, CORRELATION_ID, DURATION_MS, IDENTITY, LOCATION,
-                                  OPERATION_NAME, OPERATION_VERSION, PROPERTIES, PROPERTIES_APP_DISPLAY_NAME, PROPERTIES_APP_ID,
-                                  PROPERTIES_APPLIED_CONDITIONAL_ACESS_POLICIES, PROPERTIES_AUTHENTICATION_METHODS_USED,
-                                  PROPERTIES_AUTHENTICATION_PROCESSING_DETAILS, PROPERTIES_CLIENT_APP_USED,
-                                  PROPERTIES_CONDITIONAL_ACCESS_STATUS, PROPERTIES_CREATED_DATE_TIME, PROPERTIES_DEVICE_DETAIL,
-                                  PROPERTIES_ID, PROPERTIES_IP_ADDRESS, PROPERTIES_IS_INTERACTIVE, PROPERTIES_LOCATION,
-                                  PROPERTIES_MFA_DETAIL, PROPERTIES_NETWORK_LOCATION, PROPERTIES_PROCESSING_TIME_IN_MILLISECONDS,
-                                  PROPERTIES_RESOURCE_DISPLAY_NAME, PROPERTIES_RESOURCE_ID, PROPERTIES_RISK_DETAIL,
-                                  PROPERTIES_RISK_EVENT_TYPES, PROPERTIES_RISK_LEVEL_AGGREGATED, PROPERTIES_RISK_LEVEL_DURING_SIGNIN,
-                                  PROPERTIES_RISK_STATE, PROPERTIES_STATUS, PROPERTIES_TOKEN_ISSUER_TYPE, PROPERTIES_USER_DISPLAY_NAME,
-                                  PROPERTIES_USER_ID, PROPERTIES_USER_PRINCIPAL_NAME, RESOURCE_ID, RESULT_DESCRIPTION, RESULT_SIGNATURE,
-                                  RESULT_TYPE, TENANT_ID, EVENT_TIME, LOADED_ON)
+COPY INTO DATA.{name}_CONNECTION (
+    RAW, HASH_RAW, LEVEL, CALLER_IP_ADDRESS, CATEGORY, CORRELATION_ID, DURATION_MS,
+    IDENTITY, LOCATION, OPERATION_NAME, OPERATION_VERSION, PROPERTIES,
+    PROPERTIES_APP_DISPLAY_NAME, PROPERTIES_APP_ID,
+    PROPERTIES_APPLIED_CONDITIONAL_ACESS_POLICIES, PROPERTIES_AUTHENTICATION_METHODS_USED,
+    PROPERTIES_AUTHENTICATION_PROCESSING_DETAILS, PROPERTIES_CLIENT_APP_USED,
+    PROPERTIES_CONDITIONAL_ACCESS_STATUS, PROPERTIES_CREATED_DATE_TIME,
+    PROPERTIES_DEVICE_DETAIL, PROPERTIES_ID, PROPERTIES_IP_ADDRESS, PROPERTIES_IS_INTERACTIVE, PROPERTIES_LOCATION,
+    PROPERTIES_MFA_DETAIL, PROPERTIES_NETWORK_LOCATION, PROPERTIES_PROCESSING_TIME_IN_MILLISECONDS,
+    PROPERTIES_RESOURCE_DISPLAY_NAME, PROPERTIES_RESOURCE_ID, PROPERTIES_RISK_DETAIL,
+    PROPERTIES_RISK_EVENT_TYPES, PROPERTIES_RISK_LEVEL_AGGREGATED, PROPERTIES_RISK_LEVEL_DURING_SIGNIN,
+    PROPERTIES_RISK_STATE, PROPERTIES_STATUS, PROPERTIES_TOKEN_ISSUER_TYPE, PROPERTIES_USER_DISPLAY_NAME,
+    PROPERTIES_USER_ID, PROPERTIES_USER_PRINCIPAL_NAME, RESOURCE_ID, RESULT_DESCRIPTION, RESULT_SIGNATURE,
+    RESULT_TYPE, TENANT_ID, EVENT_TIME, LOADED_ON
+)
 FROM (
- SELECT $1, HASH($1), $1:Level::NUMBER, $1:callerIpAddress::STRING, $1:category::STRING, $1:correlationId::STRING, $1:durationMs,
-        $1:identity::STRING, $1:location::STRING, $1:operationName::STRING, $1:operationVersion::STRING, $1:properties::VARIANT,
-        $1:properties.appDisplayName::STRING, $1:properties.appId::STRING, $1:properties.appliedConditionalAccessPolicies::VARIANT,
-        $1:properties.authenticationMethodsUsed::VARIANT, $1:properties.authenticationProcessingDetails::VARIANT,
-        $1:properties.clientAppUsed::STRING, $1:properties.conditionalAccessStatus::STRING, $1:properties.createdDateTime::TIMESTAMP_LTZ,
-        $1:properties.deviceDetail::VARIANT, $1:properties.id::STRING, $1:properties.ipAddress::STRING, $1:properties.isInteractive::BOOLEAN,
-        $1:properties.location::VARIANT, $1:properties.mfaDetail::VARIANT, $1:properties.networkLocationDetails::VARIANT,
-        $1:properties.processingTimeInMilliseconds::NUMBER, $1:properties.resourceDisplayName::STRING, $1:properties.resourceId::STRING,
-        $1:properties.riskDetail::STRING, $1:properties.riskEventTypes::VARIANT, $1:properties.riskLevelAggregated::STRING,
+    SELECT $1, HASH($1), $1:Level::NUMBER, $1:callerIpAddress::STRING, $1:category::STRING, $1:correlationId::STRING,
+        $1:durationMs, $1:identity::STRING, $1:location::STRING, $1:operationName::STRING, $1:operationVersion::STRING,
+        $1:properties::VARIANT, $1:properties.appDisplayName::STRING, $1:properties.appId::STRING,
+        $1:properties.appliedConditionalAccessPolicies::VARIANT, $1:properties.authenticationMethodsUsed::VARIANT,
+        $1:properties.authenticationProcessingDetails::VARIANT, $1:properties.clientAppUsed::STRING,
+        $1:properties.conditionalAccessStatus::STRING, $1:properties.createdDateTime::TIMESTAMP_LTZ,
+        $1:properties.deviceDetail::VARIANT, $1:properties.id::STRING, $1:properties.ipAddress::STRING,
+        $1:properties.isInteractive::BOOLEAN, $1:properties.location::VARIANT, $1:properties.mfaDetail::VARIANT,
+        $1:properties.networkLocationDetails::VARIANT, $1:properties.processingTimeInMilliseconds::NUMBER,
+        $1:properties.resourceDisplayName::STRING, $1:properties.resourceId::STRING, $1:properties.riskDetail::STRING,
+        $1:properties.riskEventTypes::VARIANT, $1:properties.riskLevelAggregated::STRING,
         $1:properties.riskLevelDuringSignIn::STRING, $1:properties.riskState::VARIANT, $1:properties.status::VARIANT,
         $1:properties.tokenIssuerType::STRING, $1:properties.userDisplayName::STRING, $1:properties.userId::STRING,
-        $1:properties.userPrincipalName::STRING, $1:resourceId::STRING, $1:resultDescription::STRING, $1:resultSignature::STRING,
-        $1:resultType::STRING, $1:tenantId::STRING, $1:time::TIMESTAMP_LTZ, CURRENT_TIMESTAMP()
-FROM @DATA.{name}_STAGE
+        $1:properties.userPrincipalName::STRING, $1:resourceId::STRING, $1:resultDescription::STRING,
+        $1:resultSignature::STRING, $1:resultType::STRING, $1:tenantId::STRING, $1:time::TIMESTAMP_LTZ,
+        CURRENT_TIMESTAMP()
+    FROM @DATA.{name}_STAGE
 )
 """
     }
@@ -283,14 +295,10 @@ FROM @DATA.{name}_STAGE
 
 
 def ingest(name, options):
-    try:
-        environ['SNOWFLAKE_ACCOUNT']
-    except KeyError:
-        raise RequiredOptionMissing('SNOWFLAKE_ACCOUNT')
-    try:
-        environ['SA_USER']
-    except KeyError:
-        raise RequiredOptionMissing('SA_USER')
+    required_envars = {'SA_USER', 'SNOWFLAKE_ACCOUNT'}
+    missing_envars = required_envars - set(environ)
+    if missing_envars:
+        raise RuntimeError(f'Missing required env variable(s): {missing_envars}')
 
     block_blob_service = BlockBlobService(
         account_name=options['storage_account'],
@@ -326,11 +334,13 @@ def ingest(name, options):
     log.info(f"Found {len(new_files)} files to ingest")
 
     # Proxy object that abstracts the Snowpipe REST API
-    ingest_manager = SimpleIngestManager(account=environ.get('SNOWFLAKE_ACCOUNT'),
-                                         host=f'{environ.get("SNOWFLAKE_ACCOUNT")}.snowflakecomputing.com',
-                                         user=environ.get('SA_USER'),
-                                         pipe=f'{DATABASE}.DATA.{base_name}_PIPE',
-                                         private_key=load_pkb_rsa(PRIVATE_KEY, PRIVATE_KEY_PASSWORD))
+    ingest_manager = SimpleIngestManager(
+        account=environ.get('SNOWFLAKE_ACCOUNT'),
+        host=f'{environ.get("SNOWFLAKE_ACCOUNT")}.snowflakecomputing.com',
+        user=environ.get('SA_USER'),
+        pipe=f'{DATABASE}.DATA.{base_name}_PIPE',
+        private_key=load_pkb_rsa(PRIVATE_KEY, PRIVATE_KEY_PASSWORD)
+    )
 
     if len(new_files) > 0:
         try:
