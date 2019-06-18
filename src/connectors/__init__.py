2e1ede5343ffd29dd81e284185f97e55b9d9cb3d
Andrey Fedorov <andrey.fedorov@snowflake.com>
2019-06-18 11:32:10 -0700

    Improves connectors logic
    requires connectors to have callable "connect" function, and at least one option with a name

--- src/connectors/__init__.py
+++ src/connectors/__init__.py
@@ -13,8 +13,11 @@ connectors = {
 CONNECTION_OPTIONS = [
     {
         'connector': name,
-        'options': getattr(connector, 'CONNECTION_OPTIONS', {}),
+        'options': getattr(connector, 'CONNECTION_OPTIONS'),
         'docstring': connector.__doc__,
         'finalize': callable(getattr(connector, 'finalize', None)),
-    } for name, connector in connectors.items()
+    } for name, connector in connectors.items() if (
+        getattr(connector, 'CONNECTION_OPTIONS', [{}])[0].get('name')
+        and callable(getattr(connector, 'connect', None))
+    )
 ]
