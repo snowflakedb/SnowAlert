78b4f201305f9c5196986c6346de9c49df4ab54e
Greg Butzi <gregory.butzi@snowflake.com>
2019-06-28 08:12:46 -0700

    removes json_dumps from import

--- src/ingestion/ec2_describe_instances.py
+++ src/ingestion/ec2_describe_instances.py
@@ -7,7 +7,7 @@ import os
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
 from multiprocessing import Pool
-from runners.utils import groups_of, json_dumps
+from runners.utils import groups_of
 from runners.helpers import db
 
 import snowflake.connector
