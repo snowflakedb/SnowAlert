75c14d92f3abcddea811734a221b2222642b3276
Greg Butzi <gregory.butzi@snowflake.com>
2019-07-05 10:24:46 -0700

    Pulled sts_assume_role into utils.py

---
+++ src/connectors/utils.py
@@ -0,0 +1,20 @@
+from base64 import b64encode
+import boto3
+from os import urandom
+
+
+def sts_assume_role(src_role_arn, dest_role_arn, dest_external_id):
+    session_name = b64encode(urandom(18)).decode('utf-8')
+    src_role = boto3.client('sts').assume_role(
+        RoleArn=src_role_arn,
+        RoleSessionName=session_name
+    )
+    return boto3.Session(
+        aws_access_key_id=src_role['Credentials']['AccessKeyId'],
+        aws_secret_access_key=src_role['Credentials']['SecretAccessKey'],
+        aws_session_token=src_role['Credentials']['SessionToken']
+    ).client('sts').assume_role(
+        RoleArn=dest_role_arn,
+        RoleSessionName=session_name,
+        ExternalId=dest_external_id
+    )
