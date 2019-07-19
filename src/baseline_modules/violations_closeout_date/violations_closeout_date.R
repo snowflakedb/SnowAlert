1c3d49daaaca805f848dc4d8bc9579d6eb005fd7
snowflakemaria <44588747+snowflakemaria@users.noreply.github.com>
2019-07-18 20:44:40 -0700

    R crash debug (#220)
    * Added print statements, removed suppression for libraries
    
    * Requested new library installs in dockerfile
    
    * Closeout fix
    
    * Prediction corrections

--- src/baseline_modules/violations_closeout_date/violations_closeout_date.R
+++ src/baseline_modules/violations_closeout_date/violations_closeout_date.R
@@ -7,37 +7,54 @@
 #PROD
 #QUERY_ID
 #UNIQUE_KEYS
+#CREATE TABLE SNOWALERT.DATA.closeout_VIOLATION_QUERY_ID_BASELINE (QUERY_ID VARCHAR, TITLE VARCHAR, UNKNOWN_END VARCHAR, CLOSEOUT_DATE VARCHAR) comment = '---
+# log source: SNOWALERT.data.HOLDER_FOR_PREDICTION
+# required values:
+  # NEW: NEW
+  # FINAL: FINAL
+  # CURRENT_DAY: CURRENT_DAY
+  # PROD: PROD
+  # QUERY_ID: QUERY_ID
+  # UNIQUE_KEYS: UNIQUE_KEYS
+  # TITLE: TITLE
+# module name: violations_closeout_date
+# history: CURRENT_DAY
+# filter: 30
+# '
 
-
-      require(dplyr)
-      require(tidyverse)
-      require(broom)
-      require(MASS)
+suppressWarnings(
+  suppressMessages(
+    c(
+    require(dplyr),
+    require(tidyverse),
+    require(broom),
+    require(MASS)
+    )
+    )
+)
  
-a <- input
-rm(input)
-#Cleaning
-print('Input in a')
+a <- input_table
+rm(input_table)
+
+
 a$CURRENT_DAY <- a$CURRENT_DAY <- as.Date(as.POSIXct(a$CURRENT_DAY), format='%Y-%m-%d')
 a$FINAL <- as.logical(a$FINAL)
 a$NEW <- as.logical(a$NEW)
 a$PROD <- as.logical(a$PROD)
 colnames(a) <- make.unique(names(a))
-print('data cleaning done')
 #Group for counts
-b <- a %>% group_by(QUERY_ID, CURRENT_DAY) %>%
-  dplyr::summarize(counts=n_distinct(UNIQUE_KEYS))
-names <- a %>% group_by(QUERY_ID) %>% dplyr::summarise(TITLE=first(TITLE))
-print('b is created, a is deleted')
+b <- a %>% group_by(QUERY_ID, CURRENT_DAY) %>% dplyr::summarize(counts=n_distinct(UNIQUE_KEYS))
+namessss <- a %>% group_by(QUERY_ID) %>% dplyr::summarize(TITLE=first(TITLE))
+
 rm(a)
 #Complete the missing values with zero -> no violations
 c <- b %>% 
   tidyr::complete(CURRENT_DAY=seq.Date(min(b$CURRENT_DAY), max(b$CURRENT_DAY), by="day"),QUERY_ID, fill=list(counts = 0))
 c$age = as.integer(Sys.Date() - c$CURRENT_DAY+1)
 rm(b)
-print('completed dates for c')
+
 #Group for name
-c <- base::merge(c, names, by = "QUERY_ID", all.x=TRUE)
+c <- base::merge(c, namessss, by = "QUERY_ID", all.x=TRUE)
 
 #Do the prediction analysis
 model <- c %>% nest(-QUERY_ID) %>% 
@@ -55,12 +72,11 @@ prediction <-
   inner_join(nested, by = "QUERY_ID") %>% 
   mutate(results=map2(.x = model$fit, .y = nested$data, .f = ~augment(.x, newdata = .y), .id=.x), model2=model$fit) %>% 
   unnest(c(results))
-print('prediction complete')
-prediction <- base::merge(prediction, names, by = "QUERY_ID", all.x=TRUE)
+
+prediction <- base::merge(prediction, namessss, by = "QUERY_ID", all.x=TRUE)
+
 prediction <- base::merge(prediction, dplyr::select(model, QUERY_ID, fit), by = "QUERY_ID", all.x=TRUE)
-
 prediction$near_zero <- abs(prediction$.fitted)
-print('ready to return')
-return_value <- prediction %>% group_by(QUERY_ID) %>% summarise(last_day=max(CURRENT_DAY), x_intercept=CURRENT_DAY[which.min(near_zero)] , unknown=x_intercept==last_day, value=min(near_zero), TITLE=first(TITLE.y)) %>% dplyr::select(QUERY_ID, TITLE, unknown, x_intercept)
 
-#END
+return_value <- prediction %>% group_by(QUERY_ID) %>% summarise(last_day=max(CURRENT_DAY), x_intercept=as.character(CURRENT_DAY[which.min(near_zero)]) , unknown=as.character(x_intercept==last_day), value=min(near_zero), TITLE=first(TITLE.y)) %>% dplyr::select(QUERY_ID, TITLE, unknown, x_intercept) 
+
