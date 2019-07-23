5c2f9172f712ba7f24d145b8b464e05405b5bf54
Andrey Fedorov <andrey.fedorov@snowflake.com>
2019-07-23 15:43:26 -0700

    Adds run_id to Azure VM DC

--- src/connectors/azure_vm.py
+++ src/connectors/azure_vm.py
@@ -6,9 +6,10 @@ from azure.common.credentials import ServicePrincipalCredentials
 from azure.mgmt.compute import ComputeManagementClient
 from azure.mgmt.network import NetworkManagementClient
 
+from runners.config import RUN_ID
 from runners.helpers import db
-from runners.utils import groups_of
 from runners.helpers.dbconfig import ROLE as SA_ROLE
+from runners.utils import groups_of
 from .utils import create_metadata_table
 
 from datetime import datetime
@@ -113,7 +114,8 @@ def connect(connection_name, options):
 
     cols = [
         ('SNAPSHOT_AT', 'TIMESTAMP_LTZ'),
-        ('SUBSCRIPTION_ID', 'VARCHAR'),
+        ('RUN_ID', 'STRING(100)'),
+        ('SUBSCRIPTION_ID', 'STRING(500)'),
         ('VM_INSTANCE_COUNT', 'NUMBER')
     ]
     create_metadata_table(AZURE_COLLECTION_METADATA, cols, cols[2])
@@ -126,7 +128,7 @@ def connect(connection_name, options):
 
 def ingest(table_name, options):
     table_name = f'data.{table_name}'
-    timestamp = datetime.utcnow()
+    now = datetime.utcnow()
     client_id = options['client_id']
     secret = options['client_secret']
     tenant = options['tenant_id']
@@ -142,8 +144,8 @@ def ingest(table_name, options):
         vms = get_vms(creds, sub_id)
         db.insert(
             table=AZURE_COLLECTION_METADATA,
-            values=[(timestamp, sub_id, len(vms))],
-            columns=['SNAPSHOT_AT', 'SUBSCRIPTION_ID', 'VM_INSTANCE_COUNT'],
+            values=[(now, RUN_ID, sub_id, len(vms))],
+            columns=['SNAPSHOT_AT', 'RUN_ID', 'SUBSCRIPTION_ID', 'VM_INSTANCE_COUNT'],
         )
         for nic in get_nics(creds, sub_id):
             enrich_vms_with_nic(vms, nic)
@@ -151,7 +153,7 @@ def ingest(table_name, options):
 
     virtual_machines = [(
         elem,
-        timestamp,
+        now,
         elem.get('hardware_profile'),
         elem.get('id'),
         elem.get('location'),
