a875095e1c414735c9aae8cb9abce6ae9c0bc134
Greg Butzi <gregory.butzi@snowflake.com>
2019-07-24 12:36:46 -0700

    [DC] Removing references to aws_account.py from connectors/__init__

--- src/connectors/__init__.py
+++ src/connectors/__init__.py
@@ -4,14 +4,12 @@ from . import azure_log
 from . import azure_subscription
 from . import azure_vm
 from . import aws_config
-from . import aws_account
 from . import aws_asset_ingest
 
 __all__ = [
     'aws_asset_ingest',
     'aws_cloudtrail',
     'aws_config',
-    'aws_account',
     'azure_log',
     'azure_subscription',
     'azure_vm',
@@ -22,7 +20,6 @@ connectors = {
     'aws_asset_ingest': aws_asset_ingest,
     'aws_cloudtrail': aws_cloudtrail,
     'aws_config': aws_config,
-    'aws_account': aws_account,
     'azure_log': azure_log,
     'azure_subscription': azure_subscription,
     'azure_vm': azure_vm,
