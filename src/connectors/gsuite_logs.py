27a4547ee35c3603bb722ce104bef1c9b49c7a18
Andrey Fedorov <andrey.fedorov@snowflake.com>
2019-07-31 14:56:15 -0700

    [DC] Adds G Suite Logs Connector

---
+++ src/connectors/gsuite_logs.py
@@ -0,0 +1,139 @@
+"""G Suite Admin Logs
+Collect G Suite API logs using a Service Account or Delegated Credentials
+"""
+
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+
+from runners.helpers import db
+from runners.helpers.dbconfig import ROLE as SA_ROLE
+
+from .utils import yaml_dump
+
+CONNECTION_OPTIONS = [
+    {
+        'name': 'connection_type',
+        'type': 'select',
+        'options': [
+            {'value': 'login', 'label': "Logins"},
+        ],
+        'title': "Admin Logs Type",
+        'prompt': "The type of G Suite logs you are looking to collect.",
+        'required': True,
+        'default': 'login',
+    },
+    {
+        'name': 'service_user_creds',
+        'type': 'json',
+        'title': "Service User Credentials",
+        'prompt': "JSON Credentials from the Service User",
+        'placeholder': (
+            """{"type": "service_account", "project_id": "my-gcp-project", """
+            """"private_key_id": "34d2d9933e3f4a9ee55b968758e5ca3c2b348221", """
+            """"private_key": "-----BEGIN PRIVATE KEY-----\n...-----END PRIVATE KEY-----\n", """
+            """... }"""
+        ),
+        'required': True,
+        'secret': True,
+    },
+    {
+        'type': 'list',
+        'name': 'subjects_list',
+        'title': "List of Subjects (optional)",
+        'prompt': "Comma-separated list credentials delegated to Service Account",
+        'placeholder': "90d501a2-7c37-4036-af29-1e7e087437",
+    },
+]
+
+LANDING_TABLES_COLUMNS = {
+    'login': [
+        ('created_on', 'TIMESTAMP_LTZ'),
+        ('event_time', 'TIMESTAMP_LTZ'),
+        ('raw', 'VARIANT'),
+        ('delegating_subject', 'STRING(500)'),
+        ('etag', 'STRING(100)'),
+        ('customer_id', 'STRING(100)'),
+        ('actor_email', 'STRING(1000)'),
+        ('actor_profile_id', 'STRING(1000)'),
+        ('ip_address', 'STRING(100)'),
+    ],
+}
+
+LOGIN_EVENTS = ['logout', 'login_challenge', 'login_failure', 'login_verification', 'login_success']
+SCOPES = ['https://www.googleapis.com/auth/admin.reports.audit.readonly']
+
+
+def connect(connection_name, options):
+    connection_type = options['connection_type']
+    base_name = f'gsuite_logs_{connection_name}_{connection_type}'
+    landing_table = f'data.{base_name}_connection'
+    options['subjects_list'] = options['subjects_list'].split(',')
+    comment = yaml_dump(
+        module='gsuite_logs',
+        **options
+    )
+    db.create_table(
+        name=landing_table,
+        cols=LANDING_TABLES_COLUMNS['login'],
+        comment=comment,
+    )
+    db.execute(f'GRANT INSERT, SELECT ON data.{base_name}_connection TO ROLE {SA_ROLE}')
+
+    return {
+        'newStage': 'finalized',
+        'newMessage': 'Landing table created for collectors to populate.'
+    }
+
+
+def get_logs(service_account_info, with_subject=None, event_name='', start_time=None):
+    creds = service_account.Credentials.from_service_account_info(service_account_info)
+    if with_subject is not None:
+        creds = creds.with_subject(with_subject).with_scopes(SCOPES)
+
+    service = build('admin', version='reports_v1', credentials=creds)
+
+    return service.activities().list(
+        userKey='all',
+        applicationName='login',
+        eventName=event_name,
+        startTime=start_time and start_time.isoformat()
+    ).execute()
+
+
+def ingest(table_name, options):
+    landing_table = f'data.{table_name}'
+    service_user_creds = options['service_user_creds']
+    for subject in options.get('subjects_list') or [None]:
+        for event in LOGIN_EVENTS:
+            items = get_logs(
+                service_user_creds,
+                with_subject=subject,
+                event_name=event,
+                start_time=db.fetch_latest(landing_table)
+            ).get('items', [])
+
+            db.insert(
+                landing_table,
+                values=[(
+                    item['id']['time'],
+                    item,
+                    subject,
+                    item['etag'].strip('"'),
+                    item['id']['customerId'],
+                    item['actor'].get('email'),
+                    item['actor'].get('profileId'),
+                    item.get('ipAddress'),
+                ) for item in items],
+                select=(
+                    'CURRENT_TIMESTAMP()',
+                    'column1',
+                    'PARSE_JSON(column2)',
+                    'column3',
+                    'column4',
+                    'column5',
+                    'column6',
+                    'column7',
+                    'column8',
+                )
+            )
+            yield len(items)
