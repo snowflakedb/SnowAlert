ca80ceba3893a1634c6286fc15765d4639ba2803
Greg Butzi <gregory.butzi@snowflake.com>
2019-08-01 10:06:53 -0700

    Refactored test from Andrey's feedback

--- src/runners/tests/regression/SP1288_snowflake_url_parsing.py
+++ src/runners/tests/regression/SP1288_snowflake_url_parsing.py
@@ -1,39 +1,26 @@
 """
-The installer doesn't handle snowflake URLs with a region split by .,
+The installer didn't handle snowflake URLs with a region containing '.',
 such as azure_account.azure_region.azure.snowflakecomputing.com
 """
 
 from scripts.install import parse_snowflake_url
 
 
-TEST_URLS = [
-    "https://account.snowflakecomputing.com",
-    "account.snowflakecomputing.com",
-    "account",
-    "https://account.region.snowflakecomputing.com",
-    "account.region.snowflakecomputing.com",
-    "account.region",
-    "https://azure_account.azure_region.azure.snowflakecomputing.com",
-    "azure_account.azure_region.azure.snowflakecomputing.com",
-    "azure_account.azure_region.azure"
-]
-
-TEST_URL_RESULTS = [
-    ['account', 'us-west-2'],
-    ['account', 'us-west-2'],
-    ['account', None],
-    ['account', 'region'],
-    ['account', 'region'],
-    ['account', 'region'],
-    ['azure_account', 'azure_region.azure'],
-    ['azure_account', 'azure_region.azure'],
-    ['azure_account', 'azure_region.azure']
-
-]
+TEST_URLS = {
+    "https://account.snowflakecomputing.com": ['account', 'us-west-2'],
+    "account.snowflakecomputing.com": ['account', 'us-west-2'],
+    "account": ['account', None],
+    "https://account.region.snowflakecomputing.com": ['account', 'region'],
+    "account.region.snowflakecomputing.com": ['account', 'region'],
+    "account.region": ['account', 'region'],
+    "https://azure_account.azure_region.azure.snowflakecomputing.com": ['azure_account', 'azure_region.azure'],
+    "azure_account.azure_region.azure.snowflakecomputing.com": ['azure_account', 'azure_region.azure'],
+    "azure_account.azure_region.azure": ['azure_account', 'azure_region.azure'],
+}
 
 
 def test_url_parsing():
-    for url, results in zip(TEST_URLS, TEST_URL_RESULTS):
-        a, b = parse_snowflake_url(url)
-        assert a == results[0]
-        assert b == results[1]
+    for key, value in TEST_URLS.items():
+        a, b = parse_snowflake_url(key)
+        assert a == value[0]
+        assert b == value[1]
