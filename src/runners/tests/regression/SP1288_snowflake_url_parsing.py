1ea9358fe7c63eecbede790493b7a731d6544219
greg-snowflake <38918502+greg-snowflake@users.noreply.github.com>
2019-08-01 10:13:34 -0700

    [Installer] Adds support for Azure URL Parsing
    * parse_snowflake_url handles regions better

---
+++ src/runners/tests/regression/SP1288_snowflake_url_parsing.py
@@ -0,0 +1,26 @@
+"""
+The installer didn't handle snowflake URLs with a region containing '.',
+such as azure_account.azure_region.azure.snowflakecomputing.com
+"""
+
+from scripts.install import parse_snowflake_url
+
+
+TEST_URLS = {
+    "account": ['account', None],
+    "account.snowflakecomputing.com": ['account', 'us-west-2'],
+    "account.region": ['account', 'region'],
+    "account.region.snowflakecomputing.com": ['account', 'region'],
+    "azure_account.azure_region.azure": ['azure_account', 'azure_region.azure'],
+    "azure_account.azure_region.azure.snowflakecomputing.com": ['azure_account', 'azure_region.azure'],
+    "https://account.snowflakecomputing.com": ['account', 'us-west-2'],
+    "https://account.region.snowflakecomputing.com": ['account', 'region'],
+    "https://azure_account.azure_region.azure.snowflakecomputing.com/console": ['azure_account', 'azure_region.azure'],
+}
+
+
+def test_url_parsing():
+    for key, value in TEST_URLS.items():
+        a, b = parse_snowflake_url(key)
+        assert a == value[0]
+        assert b == value[1]
