de2211577be77a87be8ba96b916af83cad069105
greg-snowflake <38918502+greg-snowflake@users.noreply.github.com>
2019-08-12 13:35:04 -0700

    [DC] AWS Accounts Connector (#262)
    * Adds AWS Accounts Connector, using AWS Organizations API.

--- src/connectors/utils.py
+++ src/connectors/utils.py
@@ -1,6 +1,5 @@
-from base64 import b64encode
 import boto3
-from os import urandom
+import random
 import yaml
 
 from runners.helpers import db
@@ -8,12 +7,12 @@ from runners.helpers.dbconfig import ROLE as SA_ROLE
 
 
 def sts_assume_role(src_role_arn, dest_role_arn, dest_external_id=None):
-    session_name = b64encode(urandom(18)).decode('utf-8')
+    session_name = ''.join(random.choice('0123456789ABCDEF') for i in range(16))
     src_role = boto3.client('sts').assume_role(
         RoleArn=src_role_arn,
         RoleSessionName=session_name
     )
-    return boto3.Session(
+    sts_role = boto3.Session(
         aws_access_key_id=src_role['Credentials']['AccessKeyId'],
         aws_secret_access_key=src_role['Credentials']['SecretAccessKey'],
         aws_session_token=src_role['Credentials']['SessionToken']
@@ -21,6 +20,11 @@ def sts_assume_role(src_role_arn, dest_role_arn, dest_external_id=None):
         RoleArn=dest_role_arn,
         RoleSessionName=session_name,
         ExternalId=dest_external_id
+    )
+    return boto3.Session(
+        aws_access_key_id=sts_role['Credentials']['AccessKeyId'],
+        aws_secret_access_key=sts_role['Credentials']['SecretAccessKey'],
+        aws_session_token=sts_role['Credentials']['SessionToken']
     )
 
 
