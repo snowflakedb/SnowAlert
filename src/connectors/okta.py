0079b3ffbe9ca7e9685968e8d1a0ff143b05300b
greg-snowflake <38918502+greg-snowflake@users.noreply.github.com>
2019-08-23 11:04:00 -0700

    [DC] Okta Connector Enhancement (#282)
    * Enhances Okta ingestion with User and Group data

--- src/connectors/okta.py
+++ src/connectors/okta.py
@@ -4,6 +4,7 @@ Collect Okta activity logs using an API Token
 
 from runners.helpers import db, log
 from runners.helpers.dbconfig import ROLE as SA_ROLE
+from .utils import yaml_dump
 
 import datetime
 import requests
@@ -29,72 +30,123 @@ CONNECTION_OPTIONS = [
     },
 ]
 
-LANDING_TABLE_COLUMNS = [
+LANDING_LOG_TABLE_COLUMNS = [
+    ('raw', 'VARIANT'),
+    ('event_time', 'TIMESTAMP_LTZ')
+]
+
+LANDING_USER_TABLE_COLUMNS = [
+    ('raw', 'VARIANT'),
+    ('event_time', 'TIMESTAMP_LTZ')
+]
+
+LANDING_GROUP_TABLE_COLUMNS = [
     ('raw', 'VARIANT'),
     ('event_time', 'TIMESTAMP_LTZ')
 ]
 
 
 def connect(connection_name, options):
-    table_name = f'okta_{connection_name}_connection'
-    landing_table = f'data.{table_name}'
-    api_key = options['api_key']
-    subdomain = options['subdomain']
+    table_name = f'okta_{connection_name}'
+    landing_log_table = f'data.{table_name}_connection'
+    landing_user_table = f'data.{table_name}_users_connection'
+    landing_group_table = f'data.{table_name}_groups_connection'
 
-    comment = f'''
----
-module: okta
-api_key: {api_key}
-subdomain: {subdomain}
-'''
+    comment = yaml_dump(
+        module='okta',
+        **options
+    )
 
-    db.create_table(name=landing_table, cols=LANDING_TABLE_COLUMNS, comment=comment)
-    db.execute(f'GRANT INSERT, SELECT ON {landing_table} TO ROLE {SA_ROLE}')
+    db.create_table(name=landing_log_table, cols=LANDING_LOG_TABLE_COLUMNS, comment=comment)
+    db.execute(f'GRANT INSERT, SELECT ON {landing_log_table} TO ROLE {SA_ROLE}')
+
+    db.create_table(name=landing_user_table, cols=LANDING_USER_TABLE_COLUMNS, comment=comment)
+    db.execute(f'GRANT INSERT, SELECT ON {landing_user_table} TO ROLE {SA_ROLE}')
+
+    db.create_table(name=landing_group_table, cols=LANDING_GROUP_TABLE_COLUMNS, comment=comment)
+    db.execute(f'GRANT INSERT, SELECT ON {landing_group_table} TO ROLE {SA_ROLE}')
+
     return {
         'newStage': 'finalized',
-        'newMessage': "Okta ingestion table created!",
+        'newMessage': "Okta ingestion table, user table, group table created!",
     }
 
 
 def ingest(table_name, options):
+    ingest_type = ''
+    if table_name.endswith('_USERS_CONNECTION'):
+        ingest_type = 'users'
+    elif table_name.endswith('_GROUPS_CONNECTION'):
+        ingest_type = 'groups'
+    else:
+        ingest_type = 'logs'
+
     landing_table = f'data.{table_name}'
     api_key = options['api_key']
     subdomain = options['subdomain']
 
-    url = f'https://{subdomain}.okta.com/api/v1/logs'
+    url = {
+        'users': f'https://{subdomain}.okta.com/api/v1/users',
+        'groups': f'https://{subdomain}.okta.com/api/v1/groups',
+        'logs': f'https://{subdomain}.okta.com/api/v1/logs'
+    }
+
     headers = {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
         'Authorization': f'SSWS {api_key}'
     }
 
-    ts = db.fetch_latest(landing_table, 'event_time')
-    if ts is None:
-        log.error(
-            "Unable to find a timestamp of most recent Okta log, "
-            "defaulting to one hour ago"
-        )
-        ts = datetime.datetime.now() - datetime.timedelta(hours=1)
+    timestamp = datetime.datetime.utcnow()
 
-    params = {'since': ts.strftime("%Y-%m-%dT%H:%M:%S.000Z")}
-
-    while 1:
-        response = requests.get(url=url, headers=headers, params=params)
-        if response.status_code != 200:
-            log.error('OKTA REQUEST FAILED: ', response.text)
-            return
+    if ingest_type == 'groups':
+        response = requests.get(url=url[ingest_type], headers=headers)
 
         result = response.json()
-        if result == []:
-            break
+
+        for row in result:
+            row['users'] = requests.get(url=row['_links']['users']['href'], headers=headers).json()
 
         db.insert(
             landing_table,
-            values=[(row, row['published']) for row in result],
+            values=[(row, timestamp) for row in result],
             select='PARSE_JSON(column1), column2'
         )
 
         log.info(f'Inserted {len(result)} rows.')
         yield len(result)
+    else:
+        while 1:
+            response = requests.get(url=url[ingest_type], headers=headers)
+            if response.status_code != 200:
+                log.error('OKTA REQUEST FAILED: ', response.text)
+                return
 
-        url = response.headers['Link'].split(', ')[1].split(';')[0][1:-1]
+            result = response.json()
+            if result == []:
+                break
+
+            if ingest_type == 'logs':
+                db.insert(
+                    landing_table,
+                    values=[(row, row['published']) for row in result],
+                    select='PARSE_JSON(column1), column2'
+                )
+            else:
+                db.insert(
+                    landing_table,
+                    values=[(row, timestamp) for row in result],
+                    select='PARSE_JSON(column1), column2'
+                )
+
+            log.info(f'Inserted {len(result)} rows.')
+            yield len(result)
+
+            url[ingest_type] = ''
+            links = requests.utils.parse_header_links(response.headers['Link'])
+            for link in links:
+                if link['rel'] == 'next':
+                    url[ingest_type] = link['url']
+
+            if len(url[ingest_type]) == 0:
+                break
