6a824155ae6a422ee7c605c3edc26f1450ec2ca0
Greg Butzi <gregory.butzi@snowflake.com>
2019-08-29 14:31:49 -0700

    .

--- src/runners/connectors_runner.py
+++ src/runners/connectors_runner.py
@@ -20,44 +20,44 @@ def connection_run(connection_table):
     table_comment = connection_table['comment']
 
     log.info(f"-- START DC {table_name} --")
-    # try:
-    #     metadata = {'START_TIME': datetime.utcnow()}
-    #     options = yaml.load(table_comment) or {}
+    try:
+        metadata = {'START_TIME': datetime.utcnow()}
+        options = yaml.load(table_comment) or {}
 
-    #     if 'module' in options:
-    #         module = options['module']
+        if 'module' in options:
+            module = options['module']
 
-    #         metadata.update({
-    #             'RUN_ID': RUN_ID,
-    #             'TYPE': module,
-    #             'LANDING_TABLE': table_name,
-    #             'INGEST_COUNT': 0
-    #         })
+            metadata.update({
+                'RUN_ID': RUN_ID,
+                'TYPE': module,
+                'LANDING_TABLE': table_name,
+                'INGEST_COUNT': 0
+            })
 
-    #         connector = importlib.import_module(f"connectors.{module}")
+            connector = importlib.import_module(f"connectors.{module}")
 
-    #         for module_option in connector.CONNECTION_OPTIONS:
-    #             name = module_option['name']
-    #             if module_option.get('secret') and name in options:
-    #                 options[name] = vault.decrypt_if_encrypted(options[name])
-    #                 if module_option.get('type') == 'json':
-    #                     options[name] = json.loads(options[name])
+            for module_option in connector.CONNECTION_OPTIONS:
+                name = module_option['name']
+                if module_option.get('secret') and name in options:
+                    options[name] = vault.decrypt_if_encrypted(options[name])
+                    if module_option.get('type') == 'json':
+                        options[name] = json.loads(options[name])
 
-    #         if callable(getattr(connector, 'ingest', None)):
-    #             ingested = connector.ingest(table_name, options)
-    #             if isinstance(ingested, int):
-    #                 metadata['INGEST_COUNT'] += ingested
-    #             elif isinstance(ingested, GeneratorType):
-    #                 for n in ingested:
-    #                     metadata['INGEST_COUNT'] += n
-    #             else:
-    #                 metadata['INGESTED'] = ingested
+            if callable(getattr(connector, 'ingest', None)):
+                ingested = connector.ingest(table_name, options)
+                if isinstance(ingested, int):
+                    metadata['INGEST_COUNT'] += ingested
+                elif isinstance(ingested, GeneratorType):
+                    for n in ingested:
+                        metadata['INGEST_COUNT'] += n
+                else:
+                    metadata['INGESTED'] = ingested
 
-    #         db.record_metadata(metadata, table=DC_METADATA_TABLE)
+            db.record_metadata(metadata, table=DC_METADATA_TABLE)
 
-    # except Exception as e:
-    #     log.error(f"Error loading logs into {table_name}: ", e)
-    #     db.record_metadata(metadata, table=DC_METADATA_TABLE, e=e)
+    except Exception as e:
+        log.error(f"Error loading logs into {table_name}: ", e)
+        db.record_metadata(metadata, table=DC_METADATA_TABLE, e=e)
 
     log.info(f"-- END DC --")
 
