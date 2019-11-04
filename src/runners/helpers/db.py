b97d25dea7e9225a6854c9a7dbf6b266d280cd19
Andrey Fedorov <andrey.fedorov@snowflake.com>
2019-11-04 14:25:37 -0500

    [dc] Fix bugs in vault and db helpers

--- src/runners/helpers/db.py
+++ src/runners/helpers/db.py
@@ -312,12 +312,12 @@ def determine_cols(values: List[dict]) -> Tuple[List[str], List[str]]:
     selects = []  # col1, PARSE_JSON(col2), etc.
     columns = []  # col names
     for i, cname in enumerate(column_types):
-        col = column_types[cname]
+        col = column_types[cname] - {type(None)}
         if len(col) > 1:
-            log.info('col {cname} has multiple types: {col}')
+            log.info(f'col {cname} has multiple types: {col}')
             continue
 
-        ctype = col.pop()
+        ctype = col.pop() if col else type(None)
         select = f'column{i+1}'
         select = (
             f'TRY_TO_TIMESTAMP({select})'
