45b16e97f0304ee87965922c45a1eca542bc99a3
Andrey Fedorov <andrey.fedorov@snowflakecomputing.com>
2020-06-26 13:34:18 -0700

    initial attempt

---
+++ src/runners/tests/unit/service_now_handler.py
@@ -0,0 +1,14 @@
+import requests
+
+from unittest.mock import MagicMock
+from runners.handlers import service_now
+
+
+def test_handler():
+    post_mock = MagicMock(return_value={'status': 'ok'})
+    requests.post = post_mock
+
+    requests.post(asdf=21)
+    service_now.handle()
+
+    post_mock.assert_called_once()
