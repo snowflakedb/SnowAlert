913994ffbc3002b1710f79fcc19010507bbdf7c4
Andrey Fedorov <andrey.fedorov@snowflakecomputing.com>
2020-06-29 13:43:31 -0700

    fix test and handler

--- src/runners/handlers/service_now.py
+++ src/runners/handlers/service_now.py
@@ -9,6 +9,7 @@ from runners.helpers import vault
 class Bearer(requests.auth.AuthBase):
     def __init__(self, token):
         self.token = token
+
     def __call__(self, r):
         r.headers["authorization"] = "Bearer " + self.token
         return r
@@ -16,32 +17,39 @@ class Bearer(requests.auth.AuthBase):
 
 def handle(alert, assignee=''):
     host = env.get('SA_SN_API_HOST')
-    client_id = env.get('SA_SN_OAUTH_CLIENT_ID')
-    client_secret = vault.decrypt_if_encrypted(envar='SA_SN_OAUTH_CLIENT_SECRET')
-    refresh_token = vault.decrypt_if_encrypted(envar='SA_SN_OAUTH_REFRESH_TOKEN')
-    
-    oauth_return_params = {
-        'grant_type': 'refresh_token',
-        'client_id': client_id,
-        'client_secret': client_secret,
-        'refresh_token': refresh_token,
-    }
-
-    
-    oauthresp = requests.post(
-        'https://snowflakedev.service-now.com/oauth_token.do',
-        data=oauth_return_params,
-    )
-
-    result = oauthresp.json()
-    access_token = result.get('access_token')
-
     if not host:
         log.info('skipping service-now handler, missing host')
-    if not access_token:
-        log.info('skipping service-now handler, missing access token')
-        raise RuntimeError(result)
-    if not (host and username and password):
+
+    client_id = env.get('SA_SN_OAUTH_CLIENT_ID')
+    username = vault.decrypt_if_encrypted(envar='SA_SN_API_USER')
+    password = vault.decrypt_if_encrypted(envar='SA_SN_API_PASS')
+
+    client_secret = vault.decrypt_if_encrypted(envar='SA_SN_OAUTH_CLIENT_SECRET')
+    refresh_token = vault.decrypt_if_encrypted(envar='SA_SN_OAUTH_REFRESH_TOKEN')
+
+    if client_id:
+        oauth_return_params = {
+            'grant_type': 'refresh_token',
+            'client_id': client_id,
+            'client_secret': client_secret,
+            'refresh_token': refresh_token,
+        }
+
+        oauthresp = requests.post(
+            'https://snowflakedev.service-now.com/oauth_token.do',
+            data=oauth_return_params,
+        )
+
+        result = oauthresp.json()
+        access_token = result.get('access_token')
+
+        if not access_token:
+            log.info('skipping service-now handler, bad oauth')
+            raise RuntimeError(result)
+    else:
+        access_token = None
+
+    if not (username and password) and not access_token:
         return
 
     endpoint = f'https://{host}/api/now/table/incident'
@@ -49,7 +57,7 @@ def handle(alert, assignee=''):
 
     response = requests.post(
         endpoint,
-        auth=access_token,
+        auth=Bearer(access_token) if access_token else (username, password),
         data={
             'contact_type': 'Integration',
             'impact': '2',
--- src/runners/tests/unit/service_now_handler.py
+++ src/runners/tests/unit/service_now_handler.py
@@ -4,11 +4,16 @@ from unittest.mock import MagicMock
 from runners.handlers import service_now
 
 
-def test_handler():
-    post_mock = MagicMock(return_value={'status': 'ok'})
+def test_handler_success_created_201():
+    returned_mock = MagicMock(status_code=201)
+    post_mock = MagicMock(return_value=returned_mock)
+    backup_post = requests.post
     requests.post = post_mock
 
-    requests.post(asdf=21)
-    service_now.handle({})
+    try:
+        sn_handle_return_value = service_now.handle({})
+    finally:
+        requests.post = backup_post
 
     post_mock.assert_called_once()
+    assert sn_handle_return_value is returned_mock
