c5a1f3c617873248fceb507bcfab914591b99d9a
sfc-gh-afedorov <42752788+sfc-gh-afedorov@users.noreply.github.com>
2020-06-30 16:17:39 -0700

    [handler] Add OAuth to ServiceNow + unit test (#409)
    Co-authored-by: Arnav Verma <arnav.verma@snowflake.com>
    Co-authored-by: Andrey Fedorov <andrey.fedorov@snowflakecomputing.com>

--- src/runners/handlers/service_now.py
+++ src/runners/handlers/service_now.py
@@ -6,27 +6,58 @@ from runners.helpers import log
 from runners.helpers import vault
 
 
+class Bearer(requests.auth.AuthBase):
+    def __init__(self, token):
+        self.token = token
+
+    def __call__(self, r):
+        r.headers["authorization"] = "Bearer " + self.token
+        return r
+
+
 def handle(alert, assignee=''):
     host = env.get('SA_SN_API_HOST')
+    if not host:
+        log.info('skipping service-now handler, missing host')
+
     username = vault.decrypt_if_encrypted(envar='SA_SN_API_USER')
     password = vault.decrypt_if_encrypted(envar='SA_SN_API_PASS')
 
-    if not host:
-        log.info('skipping service-now handler, missing host')
-    if not username:
-        log.info('skipping service-now handler, missing username')
-    if not password:
-        log.info('skipping service-now handler, missing password')
+    client_id = env.get('SA_SN_OAUTH_CLIENT_ID')
+    client_secret = vault.decrypt_if_encrypted(envar='SA_SN_OAUTH_CLIENT_SECRET')
+    refresh_token = vault.decrypt_if_encrypted(envar='SA_SN_OAUTH_REFRESH_TOKEN')
 
-    if not (host and username and password):
+    if client_id:
+        oauth_return_params = {
+            'grant_type': 'refresh_token',
+            'client_id': client_id,
+            'client_secret': client_secret,
+            'refresh_token': refresh_token,
+        }
+
+        oauthresp = requests.post(
+            f'https://{host}/oauth_token.do',
+            data=oauth_return_params,
+        )
+
+        result = oauthresp.json()
+        access_token = result.get('access_token')
+
+        if not access_token:
+            log.info('skipping service-now handler, bad oauth')
+            raise RuntimeError(result)
+    else:
+        access_token = None
+
+    if not (username and password) and not access_token:
+        log.info('skipping service-now handler, no authorization')
         return
 
-    endpoint = f'https://{host}.service-now.com/api/now/table/incident'
     title = alert.get('TITLE', 'SnowAlert Generate Incident')
 
     response = requests.post(
-        endpoint,
-        auth=(username, password),
+        f'https://{host}/api/now/table/incident',
+        auth=Bearer(access_token) if access_token else (username, password),
         data={
             'contact_type': 'Integration',
             'impact': '2',
@@ -42,7 +73,7 @@ def handle(alert, assignee=''):
         log.info(
             f'Status: {response.status_code}',
             f'Headers: {response.headers}',
-            f'Error Response: {response.json()}',
+            f'Error Response: {response.text}',
         )
         raise RuntimeError(response)
 
