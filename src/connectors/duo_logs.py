51429806bbd7eb0534b40ed1450135a6b61df265
sfc-gh-afedorov <andrey.fedorov@snowflake.com>
2020-07-28 21:15:23 -0700

    Update duo_logs.py

--- src/connectors/duo_logs.py
+++ src/connectors/duo_logs.py
@@ -2,12 +2,14 @@
 Collect Duo admins using a key 
 """
 
- from runners.helpers import db, log
- from runners.helpers.dbconfig import ROLE as SA_ROLE
- from .utils import yaml_dump
-
 from datetime import datetime
+from runners.helpers import db, log
+from runners.helpers.dbconfig import ROLE
+
 import duo_client
+import fire
+
+from .utils import yaml_dump
 
 PAGE_SIZE = 500
 
@@ -37,51 +39,45 @@ CONNECTION_OPTIONS = [
         'type': 'str',
         'secret': True,
         'required': True,
-    }
+    },
 ]
 
-LANDING_ADMIN_TABLE_COLUMNS = [('raw', 'VARIANT', 'RECORDED_AT', 'TIMESTAMP_LTZ' )]
+LANDING_ADMIN_TABLE_COLUMNS = [('raw', 'VARIANT', 'RECORDED_AT', 'TIMESTAMP_LTZ')]
+
 
 def connect(connection_name, options):
-    table_name=f'duo_{connection_name}'
-    landing_admin_table=f'data.{table_name}_admins_connection'
+    table_name = f'duo_{connection_name}'
+    landing_admin_table = f'data.{table_name}_admins_connection'
 
     comment = yaml_dump(module='duo', **options)
 
     db.create_table(
-        name=landing_admin_table, cols=LANDING_ADMIN_TABLE_COLUMNS, comment=comment
+        name=landing_admin_table,
+        cols=LANDING_ADMIN_TABLE_COLUMNS,
+        comment=comment,
+        rw_role=ROLE,
     )
-    db.execute(f'GRANT INSERT, SELECT  ON {landing_admin_table} TO ROLE {SA_ROLE}')
 
     return {
         'newStage': 'finalized',
         'newMessage': "Duo ingestion admin table created!",
     }
 
-def authenticate_duo(URL, integration_key, token):
-    admin_api = duo_client.Admin(
-        ikey=integration_key,
-        skey=token,
-        host=URL,
-    )
-    return(admin_api)
-    
 
-def ingest(table_name, options): 
+def ingest(table_name, options, dryrun=False):
     landing_table = f'data.{table_name}'
     timestamp = datetime.utcnow()
 
-    URL = options['subdomain']
+    url = options['subdomain']
     integration_key = options['duo_integration_key']
     token = options['duo_key']
-    admin_api = authenticate_duo(URL, integration_key, token)
-
+    admin_api = duo_client.Admin(ikey=integration_key, skey=token, host=url)
 
     offset = 0
     while True:
         admins = admin_api.get_admins(limit=PAGE_SIZE, offset=offset)
         len_admins = len(admins)
-        if (len_admins==0):
+        if len_admins == 0:
             break
 
         db.insert(
@@ -93,3 +89,19 @@ def ingest(table_name, options):
         log.info(f'Inserted {len_admins} rows.')
         offset += len_admins
         yield len_admins
+
+
+def main(table_name, subdomain, duo_integration_key, duo_key, dryrun=False):
+    return ingest(
+        table_name,
+        {
+            'subdomain': subdomain,
+            'duo_integration_key': duo_integration_key,
+            'duo_key': duo_key,
+        },
+        dryrun=dryrun,
+    )
+
+
+if __name__ == '__main__':
+    fire(main)
