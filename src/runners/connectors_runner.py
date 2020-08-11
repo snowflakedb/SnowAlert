be4b06ce54d61523dfe8b200755915592ce164ce
sfc-gh-afedorov <andrey.fedorov@snowflake.com>
2020-08-11 12:47:27 -0700

    [dc] improve logging in runner

--- src/runners/connectors_runner.py
+++ src/runners/connectors_runner.py
@@ -35,19 +35,16 @@ def time_to_run(schedule, now) -> bool:
     """
     if schedule == '0 1-13/12':  # every 12 hours offset by 1
         if now.minute < 15 and now.hour % 12 == 1:
-            log.info('not time yet')
             return True
 
     m = re.match(r'^0 \*/([0-9]+)$', schedule)
     if m:  # every N hours
         n = int(m.group(1))
         if now.minute < 15 and now.hour % n == 0:
-            log.info('not time yet')
             return True
 
     if schedule == '0 *':  # hourly
         if now.minute < 15:
-            log.info('not time yet')
             return True
 
     return False
@@ -70,36 +67,40 @@ def connection_run(connection_table, run_now=False):
                 log.info(f"-- END DC --")
                 return
 
-        if 'module' in options:
-            module = options['module']
+        if 'module' not in options:
+            log.info(f'no module in options')
+            log.info(f"-- END DC --")
+            return
 
-            metadata.update(
-                {'RUN_ID': RUN_ID, 'TYPE': module, 'LANDING_TABLE': table_name}
-            )
+        module = options['module']
 
-            connector = importlib.import_module(f"connectors.{module}")
+        metadata.update(
+            {'RUN_ID': RUN_ID, 'TYPE': module, 'LANDING_TABLE': table_name}
+        )
 
-            for module_option in connector.CONNECTION_OPTIONS:
-                name = module_option['name']
-                if module_option.get('secret') and name in options:
-                    options[name] = vault.decrypt_if_encrypted(options[name])
-                if module_option.get('type') == 'json':
-                    options[name] = json.loads(options[name])
-                if module_option.get('type') == 'list':
-                    if type(options[name]) is str:
-                        options[name] = options[name].split(',')
-                if module_option.get('type') == 'int':
-                    options[name] = int(options[name])
+        connector = importlib.import_module(f"connectors.{module}")
 
-            if callable(getattr(connector, 'ingest', None)):
-                db.record_metadata(metadata, table=DC_METADATA_TABLE)
-                result = do_ingest(connector, table_name, options)
-                if result is not None:
-                    metadata['INGEST_COUNT'] = result
-                else:
-                    metadata['INGESTED'] = result
+        for module_option in connector.CONNECTION_OPTIONS:
+            name = module_option['name']
+            if module_option.get('secret') and name in options:
+                options[name] = vault.decrypt_if_encrypted(options[name])
+            if module_option.get('type') == 'json':
+                options[name] = json.loads(options[name])
+            if module_option.get('type') == 'list':
+                if type(options[name]) is str:
+                    options[name] = options[name].split(',')
+            if module_option.get('type') == 'int':
+                options[name] = int(options[name])
 
+        if callable(getattr(connector, 'ingest', None)):
             db.record_metadata(metadata, table=DC_METADATA_TABLE)
+            result = do_ingest(connector, table_name, options)
+            if result is not None:
+                metadata['INGEST_COUNT'] = result
+            else:
+                metadata['INGESTED'] = result
+
+        db.record_metadata(metadata, table=DC_METADATA_TABLE)
 
     except Exception as e:
         log.error(f"Error loading logs into {table_name}: ", e)
