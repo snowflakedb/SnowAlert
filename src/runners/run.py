c4fdeb54d5639a945b753a00c47ecfe17da384c3
sfc-gh-afedorov <andrey.fedorov@snowflake.com>
2020-08-21 20:04:52 -0700

    [run] Minor improvements to runners
    - more sensible run order for "all" param
    - fix running all connections sans ingest

--- src/runners/run.py
+++ src/runners/run.py
@@ -3,7 +3,6 @@
 import fire
 
 from runners import ingest_runner, connectors_runner
-from runners import baseline_runner
 
 from runners import alert_queries_runner
 from runners import alert_suppressions_runner
@@ -29,14 +28,19 @@ def main(target="all", *rule_names):
     elif target == "processor":
         alert_processor.main()
 
+    elif target in ['alert', 'alerts']:
+        for rule_name in rule_names:
+            alert_queries_runner.main(rule_name.upper())
+
+        alert_suppressions_runner.main()
+        alert_processor.main()
+        alert_dispatcher.main()
+
     elif target == "dispatcher":
         alert_dispatcher.main()
 
     elif rule_names:
         for rule_name in rule_names:
-            if rule_name.upper().endswith("_ALERT_QUERY"):
-                alert_queries_runner.main(rule_name.upper())
-
             if rule_name.upper().endswith("_ALERT_SUPPRESSION"):
                 alert_suppressions_runner.main(rule_name.upper())
 
@@ -55,23 +59,24 @@ def main(target="all", *rule_names):
     else:
         log.info(f"STARTING RUN WITH ID {RUN_ID}")
         log.info(f"got command {target}")
+        if target in ['connector', 'connectors', 'all']:
+            for rule_name in rule_names or [None]:
+                connectors_runner.main(rule_name)
+
+        if target in ['violation', 'violations', 'all']:
+            violation_queries_runner.main()
+            violation_suppressions_runner.main()
+
         if target in ['alert', 'alerts', 'all']:
             alert_queries_runner.main()
             alert_suppressions_runner.main()
             alert_processor.main()
             alert_dispatcher.main()
 
-        if target in ['violation', 'violations', 'all']:
-            violation_queries_runner.main()
-            violation_suppressions_runner.main()
-
         if target in ['ingest']:
             ingest_runner.main()
             connectors_runner.main()
 
-        if target in ['connector', 'connectors', 'all']:
-            for rule_name in rule_names:
-                connectors_runner.main(rule_name)
 
         if target in ['baseline', 'baselines', 'all']:
             baseline_runner.main()
