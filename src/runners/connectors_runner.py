12f723a89ddc084bb0385dd3b2b2a1437f906178
Prasanth <72515998+sfc-gh-pkommini@users.noreply.github.com>
2020-10-16 21:22:47 -0700

    Add azic nics (#447)

--- src/runners/connectors_runner.py
+++ src/runners/connectors_runner.py
@@ -50,7 +50,7 @@ def time_to_run(schedule, now) -> bool:
     return False
 
 
-def connection_run(connection_table, run_now=False):
+def connection_run(connection_table, run_now=False, extra_args={}):
     table_name = connection_table['name']
     table_comment = connection_table['comment']
 
@@ -58,6 +58,7 @@ def connection_run(connection_table, run_now=False):
     try:
         metadata = {'START_TIME': datetime.utcnow()}
         options = yaml.safe_load(table_comment) or {}
+        options.update(option_overrides)
 
         if 'schedule' in options:
             schedule = options['schedule']
@@ -109,7 +110,7 @@ def connection_run(connection_table, run_now=False):
     log.info(f"-- END DC --")
 
 
-def main(connection_table=None, run_now=False):
+def main(connection_table=None, run_now=False, **option_overrides):
     if connection_table is not None:
         # for a single table, we ignore schedule and run now
         run_now = True
@@ -118,7 +119,7 @@ def main(connection_table=None, run_now=False):
 
     tables = list(db.fetch(f"SHOW TABLES LIKE '{connection_table}' IN data"))
     if len(tables) == 1:
-        connection_run(tables[0], run_now=run_now)
+        connection_run(tables[0], run_now=run_now, option_overrides=option_overrides)
     else:
         Pool(DC_POOLSIZE).map(connection_run, tables)
 
