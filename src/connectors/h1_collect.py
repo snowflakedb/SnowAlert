bd47e4963bd1674db5f4ce617ae0c441aa28c103
Andrey Fedorov <andrey.fedorov@snowflake.com>
2021-01-28 10:44:27 -0800

    Update h1_collect.py
    ran it through pyblack formatter

--- src/connectors/h1_collect.py
+++ src/connectors/h1_collect.py
@@ -97,18 +97,12 @@ def calc_awarded_amount(report_data):
 def calc_awarded_bonus_amount(report_data):
     amount = 0.0
     for bounty in report_data:
-        bounty_bonus_amount = get_path(
-            bounty, 'attributes.awarded_bonus_amount', 0.0)
+        bounty_bonus_amount = get_path(bounty, 'attributes.awarded_bonus_amount', 0.0)
         amount += float(bounty_bonus_amount)
     return amount
 
 
-def load_data(
-    url: str,
-    token: str,
-    api_identifier: str,
-    params: dict = {}
-):
+def load_data(url: str, token: str, api_identifier: str, params: dict = {}):
     """Perform the API call"""
 
     headers: dict = {"Accept": "application/json"}
@@ -122,7 +116,7 @@ def load_data(
     return (
         parsedate_to_datetime(resp.headers['Date']),
         res.get('data'),
-        res.get('links', {}).get('next')
+        res.get('links', {}).get('next'),
     )
 
 
@@ -136,7 +130,9 @@ def get_path(data: dict, path: str, default=None):
 
 
 def connect(connection_name, options):
-    landing_transactions_table = f'data.h1_collect_{connection_name}_transactions_connection'
+    landing_transactions_table = (
+        f'data.h1_collect_{connection_name}_transactions_connection'
+    )
     landing_reports_table = f'data.h1_collect_{connection_name}_reports_connection'
 
     comment = yaml_dump(module='h1_collect', **options)
@@ -145,14 +141,14 @@ def connect(connection_name, options):
         name=landing_transactions_table,
         cols=LANDING_TABLE_COLUMNS_TRANSACTIONS,
         comment=comment,
-        rw_role=SA_ROLE
+        rw_role=SA_ROLE,
     )
 
     db.create_table(
         name=landing_reports_table,
         cols=LANDING_TABLE_COLUMNS_REPORTS,
         comment=comment,
-        rw_role=SA_ROLE
+        rw_role=SA_ROLE,
     )
     return {
         'newStage': 'finalized',
@@ -173,16 +169,29 @@ def insert_reports(landing_table, reports, recorded_at, dryrun):
                 'title': get_path(report, 'attributes.title'),
                 'state': get_path(report, 'attributes.state'),
                 'created_at': get_path(report, 'attributes.created_at'),
-                'vulnerability_information': get_path(report, 'attributes.vulnerability_information'),
+                'vulnerability_information': get_path(
+                    report, 'attributes.vulnerability_information'
+                ),
                 'triaged_at': get_path(report, 'attributes.triaged_at'),
                 'closed_at': get_path(report, 'attributes.closed_at'),
-                'first_program_activity_at': get_path(report, 'attributes.first_program_activity_at'),
+                'first_program_activity_at': get_path(
+                    report, 'attributes.first_program_activity_at'
+                ),
                 'bounty_awarded_at': get_path(report, 'attributes.bounty_awarded_at'),
-                'rating': get_path(report, 'relationships.severity.data.attributes.rating'),
+                'rating': get_path(
+                    report, 'relationships.severity.data.attributes.rating'
+                ),
                 'name': get_path(report, 'relationships.weakness.data.attributes.name'),
-                'description': get_path(report, 'relationships.weakness.data.attributes.description'),
-                'external_id': get_path(report, 'relationships.weakness.data.attributes.external_id'),
-                'asset_identifier': get_path(report, 'relationships.structured_scope.data.attributes.asset_identifier'),
+                'description': get_path(
+                    report, 'relationships.weakness.data.attributes.description'
+                ),
+                'external_id': get_path(
+                    report, 'relationships.weakness.data.attributes.external_id'
+                ),
+                'asset_identifier': get_path(
+                    report,
+                    'relationships.structured_scope.data.attributes.asset_identifier',
+                ),
                 'awarded_amount': calc_awarded_amount(
                     get_path(report, 'relationships.bounties.data', default=[])
                 ),
@@ -205,10 +214,14 @@ def insert_transactions(landing_table, transactions, recorded_at, dryrun):
                 'raw': transaction,
                 'type': transaction.get('type', ''),
                 'activity_date': get_path(transaction, 'attributes.activity_date'),
-                'activity_description': get_path(transaction, 'attributes.activity_description'),
+                'activity_description': get_path(
+                    transaction, 'attributes.activity_description'
+                ),
                 'bounty_award': get_path(transaction, 'attributes.bounty_award'),
                 'bounty_fee': get_path(transaction, 'attributes.bounty_fee'),
-                'debit_or_credit_amount': get_path(transaction, 'attributes.debit_or_credit_amount'),
+                'debit_or_credit_amount': get_path(
+                    transaction, 'attributes.debit_or_credit_amount'
+                ),
                 'balance': get_path(transaction, 'attributes.balance'),
                 'id': get_path(transaction, 'relationships.report.data.id'),
                 'url': get_path(transaction, 'relationships.report.links.self'),
@@ -233,7 +246,7 @@ def paginated_insert_reports(landing_table, options, dryrun):
             params={
                 'filter[program][]': program_name,
                 'page[size]': PAGE_SIZE,
-                'page[number]': page_number
+                'page[number]': page_number,
             },
         )
         page_number += 1
@@ -255,20 +268,14 @@ def paginated_insert_transactions(landing_table, options, dryrun):
                 f'https://api.hackerone.com/v1/programs/{account_id}/billing/transactions',
                 api_token,
                 api_identifier,
-                params={
-                    'page[size]': PAGE_SIZE,
-                    'month': month,
-                    'year': year
-                },
+                params={'page[size]': PAGE_SIZE, 'month': month, 'year': year},
             )
-            insert_transactions(landing_table, transactions,
-                                recorded_at, dryrun)
+            insert_transactions(landing_table, transactions, recorded_at, dryrun)
 
 
 def ingest(table_name, options, dryrun=False):
     ingest_type = (
-        'transaction' if table_name.endswith(
-            '_TRANSACTIONS_CONNECTION') else 'report'
+        'transaction' if table_name.endswith('_TRANSACTIONS_CONNECTION') else 'report'
     )
     landing_table = f'data.{table_name}'
 
