f7c53ffb59f533ffe467ce1d17e5cf88d9bae856
Andrey Fedorov <andrey.fedorov@snowflake.com>
2021-02-03 21:12:51 -0800

    Update cloudwatch_metric.py

--- infra/terraform/external_function/snowalert/lambda-code/protocols/cloudwatch_metric.py
+++ infra/terraform/external_function/snowalert/lambda-code/protocols/cloudwatch_metric.py
@@ -2,14 +2,16 @@ import boto3
 import datetime
 
 
-def cloudwatch_metric(namespace, name, dimensions, value, region='us-west-2'):
+def cloudwatch_metric(
+    namespace, name, dimensions, value, timestamp=None, region='us-west-2'
+):
     boto3.client('cloudwatch', region).put_metric_data(
         Namespace=namespace,
         MetricData=[
             {
                 'MetricName': name,
                 'Dimensions': dimensions,
-                'Timestamp': datetime.datetime.utcnow(),
+                'Timestamp': timestamp or datetime.datetime.utcnow(),
                 'Value': value,
             }
         ],
