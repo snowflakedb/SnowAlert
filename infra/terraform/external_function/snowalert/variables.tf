28c9f925cccc16ed63b595ae8d01581f08ee2356
Andrey Fedorov <andrey.fedorov@snowflake.com>
2021-02-17 07:50:03 -0800

    add region name to TF

--- infra/terraform/external_function/snowalert/variables.tf
+++ infra/terraform/external_function/snowalert/variables.tf
@@ -4,6 +4,9 @@ variable "snowflake_integration_user" {}
 variable "aws_cloudwatch_metric_namespace" {}
 
 data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
 locals {
-  account_id = data.aws_caller_identity.current.account_id
+  account_id  = data.aws_caller_identity.current.account_id
+  region_name = data.aws_region.current.region_name
 }
