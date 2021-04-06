909e49d9d766bbf58ee8df640ab6b5b8a5c8c2b7
sfc-gh-nlele <nachiket.lele@snowflake.com>
2021-04-06 19:44:25 +0530

    Adding CodeMirror 6

--- src/webui/frontend/src/actions/cmactions.ts
+++
@@ -1,7 +0,0 @@
-
-
-const updateEditorValue = (docString: string): {type: string; payload: string} => {
-    return {type: 'EDITOR_CHANGE', payload: docString};
-  };
-
-export default updateEditorValue;
