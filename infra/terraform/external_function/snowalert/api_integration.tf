4d54228edcd6438ad8ac6648db8d7449b823fc58
Chetan <chetan.nakhate@snowflake.com>
2021-04-20 21:08:41 +0530

    made changes according to review

--- infra/terraform/external_function/snowalert/api_integration.tf
+++ infra/terraform/external_function/snowalert/api_integration.tf
@@ -19,6 +19,6 @@ resource "snowflake_api_integration" "api_integration" {
   enabled = true
   api_provider = "aws_api_gateway"
   api_allowed_prefixes = [aws_api_gateway_deployment.prod.invoke_url]
-  api_aws_role_arn     = "arn:aws:iam::${local.account_id}:role/${var.gateway_caller_name}"
+  api_aws_role_arn     = "arn:aws:iam::${local.account_id}:role/${var.prefix}-api-gateway-caller"
 }
 
