27ce7ddd3672f285a52bf33adf4e793e568964cc
sfc-gh-afedorov <42752788+sfc-gh-afedorov@users.noreply.github.com>
2021-04-22 11:48:21 -0700

    Update process_https.py

--- infra/terraform/external_function/snowalert/lambda-code/drivers/process_https.py
+++ infra/terraform/external_function/snowalert/lambda-code/drivers/process_https.py
@@ -12,7 +12,7 @@ from vault import decrypt_if_encrypted
 
 
 def make_basic_header(auth):
-    return b"Basic " + b64encode(auth.encode())
+    return b'Basic ' + b64encode(auth.encode())
 
 
 def parse_header_dict(value):
@@ -21,56 +21,56 @@ def parse_header_dict(value):
 
 def process_row(
     data: Optional[str] = None,
-    base_url: str = "",
-    url: str = "",
+    base_url: str = '',
+    url: str = '',
     json: Optional[str] = None,
-    method: str = "get",
-    headers: str = "",
-    kwargs: Union[Dict, str] = "",
+    method: str = 'get',
+    headers: str = '',
+    kwargs: Union[Dict, str] = '',
     auth: str = None,
-    params: str = "",
+    params: str = '',
     verbose: bool = False,
-    cursor: str = "",
-    results_path: str = "",
+    cursor: str = '',
+    results_path: str = '',
 ):
     if url:
         req_url = base_url + url
-        m = match(r"^https://([^/]+)(.*)$", req_url)
+        m = match(r'^https://([^/]+)(.*)$', req_url)
         if m:
             req_host, req_path = m.groups()
         else:
             raise RuntimeError("url must start with https://")
     else:
         req_host = base_url
-        req_path = url or "/"
+        req_path = url or '/'
 
     req_kwargs = parse_header_dict(kwargs)
 
     req_headers = {
         k: v.format(**req_kwargs) for k, v in parse_header_dict(headers).items()
     }
-    req_headers.setdefault("User-Agent", "Snowflake SnowAlert External Function 1.0")
+    req_headers.setdefault('User-Agent', 'Snowflake SnowAlert External Function 1.0')
     if auth:
         auth = str(decrypt_if_encrypted(auth))
         req_auth = (
             loads(auth)
-            if auth.startswith("{")
+            if auth.startswith('{')
             else parse_header_dict(auth)
             if auth
             else {}
         )
 
-        if "host" in req_auth and not req_host:
-            req_host = req_auth["host"]
+        if 'host' in req_auth and not req_host:
+            req_host = req_auth['host']
 
         if req_auth.get("host") != req_host:
             pass  # if host in ct, only send creds to that host
-        elif "basic" in req_auth:
-            req_headers["Authorization"] = make_basic_header(req_auth["basic"])
-        elif "bearer" in req_auth:
-            req_headers["Authorization"] = f"Bearer {req_auth['bearer']}"
+        elif 'basic' in req_auth:
+            req_headers['Authorization'] = make_basic_header(req_auth['basic'])
+        elif 'bearer' in req_auth:
+            req_headers['Authorization'] = f"Bearer {req_auth['bearer']}"
         elif "authorization" in req_auth:
-            req_headers["authorization"] = req_auth["authorization"]
+            req_headers['authorization'] = req_auth['authorization']
 
     # query, nextpage_path, results_path
     req_params: str = params
@@ -80,13 +80,13 @@ def process_row(
 
     if json:
         req_data: Optional[bytes] = (
-            json if json.startswith("{") else dumps(parse_header_dict(json))
+            json if json.startswith('{') else dumps(parse_header_dict(json))
         ).encode()
-        req_headers["Content-Type"] = "application/json"
+        req_headers['Content-Type'] = 'application/json'
     else:
         req_data = None if data is None else data.encode()
 
-    req_url += f"?{req_params}"
+    req_url += f'?{req_params}'
     next_url: Optional[str] = req_url
     row_data: List[Any] = []
 
@@ -96,20 +96,20 @@ def process_row(
         try:
             res = urlopen(req)
             links_headers = parse_header_links(
-                ",".join(res.headers.get_all("link", []))
+                ",".join(res.headers.get_all('link', []))
             )
             response_body = res.read()
             response_headers = dict(res.getheaders())
             response_date = (
-                parsedate_to_datetime(response_headers["Date"]).isoformat()
-                if "Date" in response_headers
+                parsedate_to_datetime(response_headers['Date']).isoformat()
+                if 'Date' in response_headers
                 else None
             )
             response = (
                 {
-                    "body": loads(response_body),
-                    "headers": response_headers,
-                    "responded_at": response_date,
+                    'body': loads(response_body),
+                    'headers': response_headers,
+                    'responded_at': response_date,
                 }
                 if verbose
                 else loads(response_body)
@@ -117,36 +117,36 @@ def process_row(
             result = pick(req_results_path, response)
         except HTTPError as e:
             result = {
-                "error": f"{e.code} {e.reason}",
-                "url": next_url,
+                'error': f'{e.code} {e.reason}',
+                'url': next_url,
             }
         except URLError as e:
             result = {
-                "error": f"URLError",
-                "reason": str(e.reason),
-                "host": req_host,
+                'error': f'URLError',
+                'reason': str(e.reason),
+                'host': req_host,
             }
         except JSONDecodeError as e:
             result = {
-                "error": "JSONDecodeError",
-                "text": response_body.decode(),
+                'error': 'JSONDecodeError',
+                'text': response_body.decode(),
             }
 
         if req_cursor and isinstance(result, list):
             row_data += result
             if ":" in req_cursor:
-                cursor_path, cursor_param = req_cursor.rsplit(":", 1)
+                cursor_path, cursor_param = req_cursor.rsplit(':', 1)
             else:
                 cursor_path = req_cursor
-                cursor_param = cursor_path.split(".")[-1]
+                cursor_param = cursor_path.split('.')[-1]
             cursor_value = pick(cursor_path, response)
             next_url = (
-                f"{req_url}&{cursor_param}={cursor_value}" if cursor_value else None
+                f'{req_url}&{cursor_param}={cursor_value}' if cursor_value else None
             )
         elif links_headers and isinstance(result, list):
             row_data += result
-            link_dict: Dict[Any, Any] = next((l for l in links_headers if l["rel"] == "next"), {})
-            nu: Optional[str] = link_dict.get("url")
+            link_dict: Dict[Any, Any] = next((l for l in links_headers if l['rel'] == 'next'), {})
+            nu: Optional[str] = link_dict.get('url')
             next_url = nu if nu != next_url else None
         else:
             row_data = result
