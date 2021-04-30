0b6b57663284836e21503e70035a719635b33661
Andrey Fedorov <andrey.fedorov@snowflake.com>
2021-04-30 12:04:12 -0700

    [geff] add authorized-user-info option to google driver
    tested w/ SP on calendar API

--- infra/terraform/external_function/snowalert/lambda-code/drivers/process_google.py
+++ infra/terraform/external_function/snowalert/lambda-code/drivers/process_google.py
@@ -7,18 +7,26 @@ from vault import decrypt_if_encrypted
 
 
 def process_row(
-    service_account_info,
     service_name,
     service_version,
     resource_name,
     method,
     params,
+    service_account_info=None,
+    authorized_user_info=None,
     without_subject=False,
     subject=None,
     scopes=None,
 ):
-    c = service_account.Credentials.from_service_account_info(
-        loads(decrypt_if_encrypted(service_account_info))
+
+    c = (
+        service_account.Credentials.from_service_account_info(
+            loads(decrypt_if_encrypted(service_account_info))
+        )
+        if authorized_user_info is None
+        else service_account.Credentials.from_authorized_user_info(
+            loads(decrypt_if_encrypted(authorized_user_info))
+        )
     )
     if subject is None or without_subject:
         creds = c.with_scopes(scopes)
