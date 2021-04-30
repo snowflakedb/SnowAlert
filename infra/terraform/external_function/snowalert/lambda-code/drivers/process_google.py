2ee75fdf30ab65c0e6c92e023cebb0a548e39e34
Andrey Fedorov <andrey.fedorov@snowflake.com>
2021-04-30 12:07:15 -0700

    Update process_google.py

--- infra/terraform/external_function/snowalert/lambda-code/drivers/process_google.py
+++ infra/terraform/external_function/snowalert/lambda-code/drivers/process_google.py
@@ -1,6 +1,6 @@
 from json import loads
 
-from google.oauth2 import service_account
+from google.oauth2 import service_account, credentials
 from googleapiclient.discovery import build
 
 from vault import decrypt_if_encrypted
@@ -18,20 +18,19 @@ def process_row(
     subject=None,
     scopes=None,
 ):
+    if authorized_user_info is not None:
+        creds = credentials.Credentials.from_authorized_user_info(
+            loads(decrypt_if_encrypted(authorized_user_info)), scopes
+        )
 
-    c = (
-        service_account.Credentials.from_service_account_info(
+    else:
+        c = service_account.Credentials.from_service_account_info(
             loads(decrypt_if_encrypted(service_account_info))
         )
-        if authorized_user_info is None
-        else service_account.Credentials.from_authorized_user_info(
-            loads(decrypt_if_encrypted(authorized_user_info))
-        )
-    )
-    if subject is None or without_subject:
-        creds = c.with_scopes(scopes)
-    else:
-        creds = c.with_subject(subject).with_scopes(scopes)
+        if subject is None or without_subject:
+            creds = c.with_scopes(scopes)
+        else:
+            creds = c.with_subject(subject).with_scopes(scopes)
 
     service = build(service_name, version=service_version, credentials=creds)
     resource_name, *subresource_names = resource_name.split('.')
