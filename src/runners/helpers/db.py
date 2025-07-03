c83cd8655154d309666fdd0fdffbcd556dc46954
sfc-gh-pkommini <prasanth.kommini@snowflake.com>
2025-07-03 14:18:34 -0700

    Reduce page size further.

--- src/runners/helpers/db.py
+++ src/runners/helpers/db.py
@@ -39,7 +39,7 @@ from runners.config import DATA_SCHEMA
 CACHE = local()
 CONNECTION = f'connection-{getpid()}'
 JSONY = (dict, list, tuple, Exception, datetime)
-INSERT_BATCH_SIZE = 8000
+INSERT_BATCH_SIZE = 4000
 
 def retry(
     f,
